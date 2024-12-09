02dac053ceb3b7990421e3ec61b4cc5405e2cbe5
Le Vivilet <levivlet@pm.me>
2024-12-09 20:31:45 +0100

    fix

--- packages/title-bar-worker/src/parts/GetTextMeasureContext/GetTextMeasureContext.ts
+++ packages/title-bar-worker/src/parts/GetTextMeasureContext/GetTextMeasureContext.ts
@@ -1,6 +1,6 @@
 import * as MeasureTextWidthState from '../MeasureTextWidthState/MeasureTextWidthState.ts'
 
-const createCtx = () => {
+const createCtx = (): OffscreenCanvasRenderingContext2D => {
   const canvas = new OffscreenCanvas(0, 0)
   const ctx = /** @type {OffscreenCanvasRenderingContext2D} */ canvas.getContext('2d')
   if (!ctx) {
@@ -9,7 +9,7 @@ const createCtx = () => {
   return ctx
 }
 
-export const getContext = () => {
+export const getContext = (): OffscreenCanvasRenderingContext2D => {
   const ctx = MeasureTextWidthState.getOrCreate(createCtx)
   return ctx
 }
