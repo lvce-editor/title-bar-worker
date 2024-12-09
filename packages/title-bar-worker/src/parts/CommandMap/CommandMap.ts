9c47f029bc97153f46d0f527aec3aef9ed03443d
Le Vivilet <levivlet@pm.me>
2024-12-09 22:00:02 +0100

    sort

--- packages/title-bar-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/title-bar-worker/src/parts/CommandMap/CommandMap.ts
@@ -5,9 +5,12 @@ import * as TitleBarMenuBar from '../TitleBarMenuBar/TitleBarMenuBar.ts'
 import * as CloseMenu from '../TitleBarMenuBar/ViewletTitleBarMenuBarCloseMenu.ts'
 import * as Focus from '../TitleBarMenuBar/ViewletTitleBarMenuBarFocus.ts'
 import * as ViewletTitleBarMenuBarFocusFirst from '../TitleBarMenuBar/ViewletTitleBarMenuBarFocusFirst.ts'
+import * as ViewletTitleBarMenuBarFocusIndex from '../TitleBarMenuBar/ViewletTitleBarMenuBarFocusIndex.ts'
 import * as ViewletTitleBarMenuBarFocusLast from '../TitleBarMenuBar/ViewletTitleBarMenuBarFocusLast.ts'
 import * as ViewletTitleBarMenuBarFocusNext from '../TitleBarMenuBar/ViewletTitleBarMenuBarFocusNext.ts'
 import * as ViewletTitleBarMenuBarFocusPrevious from '../TitleBarMenuBar/ViewletTitleBarMenuBarFocusPrevious.ts'
+import * as ViewletTitleBarMenuBarHandleClick from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleClick.ts'
+import * as ViewletTitleBarMenuBarHandleFocus from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleFocus.ts'
 import * as ViewletTitleBarMenuBarHandleKeyArrowDown from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleKeyArrowDown.ts'
 import * as ViewletTitleBarMenuBarHandleKeyArrowLeft from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleKeyArrowLeft.ts'
 import * as ViewletTitleBarMenuBarHandleKeyArrowRight from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleKeyArrowRight.ts'
@@ -17,29 +20,27 @@ import * as ViewletTitleBarMenuBarHandleKeyEnter from '../TitleBarMenuBar/Viewle
 import * as ViewletTitleBarMenuBarHandleKeyEscape from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleKeyEscape.ts'
 import * as ViewletTitleBarMenuBarHandleKeyHome from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleKeyHome.ts'
 import * as ViewletTitleBarMenuBarHandleKeySpace from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleKeySpace.ts'
-import * as ViewletTitleBarMenuBarFocusIndex from '../TitleBarMenuBar/ViewletTitleBarMenuBarFocusIndex.ts'
-import * as ViewletTitleBarMenuBarHandleMenuMouseOver from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleMenuMouseOver.ts'
-import * as ViewletTitleBarMenuBarToggleIndex from '../TitleBarMenuBar/ViewletTitleBarMenuBarToggleIndex.ts'
 import * as ViewletTitleBarMenuBarHandleMenuClick from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleMenuClick.ts'
-import * as ViewletTitleBarMenuBarHandleClick from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleClick.ts'
+import * as ViewletTitleBarMenuBarHandleMenuMouseOver from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleMenuMouseOver.ts'
 import * as ViewletTitleBarMenuBarHandleMouseOut from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleMouseOut.ts'
 import * as ViewletTitleBarMenuBarHandleMouseOver from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleMouseOver.ts'
+import * as ViewletTitleBarMenuBarToggleIndex from '../TitleBarMenuBar/ViewletTitleBarMenuBarToggleIndex.ts'
 import * as ViewletTitleBarMenuBarToggleMenu from '../TitleBarMenuBar/ViewletTitleBarMenuBarToggleMenu.ts'
-import * as ViewletTitleBarMenuBarHandleFocus from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleFocus.ts'
 
 export const commandMap = {
-  'TitleBarMenuBar.create': TitleBarMenuBar.create,
-  'TitleBarMenuBar.getKeyBindings': GetKeyBindings.getKeyBindings,
-  'TitleBarMenuBar.getMenus': MenuEntries.getMenus,
-  'TitleBarMenuBar.getVirtualDom': GetTitleBarMenuBarVirtualDom.getTitleBarMenuBarVirtualDom,
-  'TitleBarMenuBar.loadContent': TitleBarMenuBar.loadContent,
   'TitleBarMenuBar.closeMenu': CloseMenu.closeMenu,
+  'TitleBarMenuBar.create': TitleBarMenuBar.create,
   'TitleBarMenuBar.focus': Focus.focus,
   'TitleBarMenuBar.focusFirst': ViewletTitleBarMenuBarFocusFirst.focusFirst,
   'TitleBarMenuBar.focusIndex': ViewletTitleBarMenuBarFocusLast.focusLast,
   'TitleBarMenuBar.focusLast': ViewletTitleBarMenuBarFocusIndex.focusIndex,
   'TitleBarMenuBar.focusNext': ViewletTitleBarMenuBarFocusNext.focusNext,
   'TitleBarMenuBar.focusPrevious': ViewletTitleBarMenuBarFocusPrevious.focusPrevious,
+  'TitleBarMenuBar.getKeyBindings': GetKeyBindings.getKeyBindings,
+  'TitleBarMenuBar.getMenus': MenuEntries.getMenus,
+  'TitleBarMenuBar.getVirtualDom': GetTitleBarMenuBarVirtualDom.getTitleBarMenuBarVirtualDom,
+  'TitleBarMenuBar.handleClick': ViewletTitleBarMenuBarHandleClick.handleClick,
+  'TitleBarMenuBar.handleFocus': ViewletTitleBarMenuBarHandleFocus.handleFocus,
   'TitleBarMenuBar.handleKeyArrowDown': ViewletTitleBarMenuBarHandleKeyArrowDown.handleKeyArrowDown,
   'TitleBarMenuBar.handleKeyArrowLeft': ViewletTitleBarMenuBarHandleKeyArrowLeft.handleKeyArrowLeft,
   'TitleBarMenuBar.handleKeyArrowRight': ViewletTitleBarMenuBarHandleKeyArrowRight.handleKeyArrowRight,
@@ -51,10 +52,9 @@ export const commandMap = {
   'TitleBarMenuBar.handleKeySpace': ViewletTitleBarMenuBarHandleKeySpace.handleKeySpace,
   'TitleBarMenuBar.handleMenuClick': ViewletTitleBarMenuBarHandleMenuClick.handleMenuClick,
   'TitleBarMenuBar.handleMenuMouseOver': ViewletTitleBarMenuBarHandleMenuMouseOver.handleMenuMouseOver,
-  'TitleBarMenuBar.handleMouseOver': ViewletTitleBarMenuBarHandleMouseOver.handleMouseOver,
   'TitleBarMenuBar.handleMouseOut': ViewletTitleBarMenuBarHandleMouseOut.handleMouseOut,
+  'TitleBarMenuBar.handleMouseOver': ViewletTitleBarMenuBarHandleMouseOver.handleMouseOver,
+  'TitleBarMenuBar.loadContent': TitleBarMenuBar.loadContent,
   'TitleBarMenuBar.toggleIndex': ViewletTitleBarMenuBarToggleIndex.toggleIndex,
   'TitleBarMenuBar.toggleMenu': ViewletTitleBarMenuBarToggleMenu.toggleMenu,
-  'TitleBarMenuBar.handleClick': ViewletTitleBarMenuBarHandleClick.handleClick,
-  'TitleBarMenuBar.handleFocus': ViewletTitleBarMenuBarHandleFocus.handleFocus,
 }
