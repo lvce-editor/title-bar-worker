5e6e57482036c1396f80df21a927f03b0c977b29
Le Vivilet <levivlet@pm.me>
2024-12-09 21:43:56 +0100

    code

--- packages/title-bar-worker/src/parts/PlatformType/PlatformType.ts
+++ packages/title-bar-worker/src/parts/PlatformType/PlatformType.ts
@@ -1,5 +1,3 @@
 export const Web = 1
 
 export const Electron = 2
-
-export const Remote = 3
--- packages/title-bar-worker/src/parts/Workspace/Workspace.ts
+++ packages/title-bar-worker/src/parts/Workspace/Workspace.ts
@@ -1,7 +1,3 @@
 export const getHomeDir = (): string => {
   return ''
 }
-
-export const pathBaseName = (path: string): string => {
-  return path.slice(path.lastIndexOf('/') + 1)
-}
