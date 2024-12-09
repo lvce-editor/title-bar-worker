573304274dd2040fbd532353da51b5b67f0a0e43
Le Vivilet <levivlet@pm.me>
2024-12-09 21:44:36 +0100

    code

--- packages/title-bar-worker/src/parts/GetIndex/GetIndex.ts
+++
@@ -1,9 +0,0 @@
-export const getIndex = (dirents: any, uri: string): number => {
-  for (let i = 0; i < dirents.length; i++) {
-    const dirent = dirents[i]
-    if (dirent.path === uri) {
-      return i
-    }
-  }
-  return -1
-}
