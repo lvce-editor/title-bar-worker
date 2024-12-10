c3e89850db7d69cadb6f563e538307e781e9f664
Le Vivilet <levivlet@pm.me>
2024-12-10 20:32:57 +0100

    tsc

--- packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarFocusIndex.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarFocusIndex.ts
@@ -1,7 +1,7 @@
 import * as Assert from '../Assert/Assert.ts'
 import { openMenuAtIndex } from './ViewletTitleBarMenuBarOpenMenuAtIndex.ts'
 
-export const focusIndex = (state, index) => {
+export const focusIndex = (state: any, index: number): any => {
   Assert.object(state)
   Assert.number(index)
   const { isMenuOpen } = state
--- packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleMouseOverMenuClosed.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleMouseOverMenuClosed.ts
@@ -1,5 +1,5 @@
 import { focusIndex } from './ViewletTitleBarMenuBarFocusIndex.ts'
 
-export const handleMouseOverMenuClosed = (state, index) => {
+export const handleMouseOverMenuClosed = (state: any, index: number): any => {
   return focusIndex(state, index)
 }
