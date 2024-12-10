0c8a70c6e3a4749926ca76b5fc3e9c21e39e7a06
Le Vivilet <levivlet@pm.me>
2024-12-10 20:46:01 +0100

    tsc

--- packages/title-bar-worker/src/parts/MenuEntries/MenuEntries.ts
+++ packages/title-bar-worker/src/parts/MenuEntries/MenuEntries.ts
@@ -2,12 +2,13 @@ import * as MenuEntriesEdit from '../MenuEntriesEdit/MenuEntriesEdit.ts'
 import * as MenuEntriesFile from '../MenuEntriesFile/MenuEntriesFile.ts'
 import * as MenuEntriesGo from '../MenuEntriesGo/MenuEntriesGo.ts'
 import * as MenuEntriesHelp from '../MenuEntriesHelp/MenuEntriesHelp.ts'
+import * as MenuEntriesOpenRecent from '../MenuEntriesOpenRecent/MenuEntriesOpenRecent.ts'
 import * as MenuEntriesRun from '../MenuEntriesRun/MenuEntriesRun.ts'
 import * as MenuEntriesSelection from '../MenuEntriesSelection/MenuEntriesSelection.ts'
 import * as MenuEntriesTerminal from '../MenuEntriesTerminal/MenuEntriesTerminal.ts'
-import * as MenuEntriesOpenRecent from '../MenuEntriesOpenRecent/MenuEntriesOpenRecent.ts'
 import * as MenuEntriesTitleBar from '../MenuEntriesTitleBar/MenuEntriesTitleBar.ts'
 import * as MenuEntriesView from '../MenuEntriesView/MenuEntriesView.ts'
+import { VError } from '../VError/VError.ts'
 
 const menus = [
   MenuEntriesEdit,
@@ -24,4 +25,16 @@ const menus = [
 
 export const getMenus = (): any => {
   return menus
+}
+
+export const getMenuEntries = async (id: any, ...args: any) => {
+  try {
+    const module = menus[0]
+    // @ts-ignore
+    const inject = module.inject || []
+    // @ts-ignore
+    return module.getMenuEntries(...args)
+  } catch (error) {
+    throw new VError(error, `Failed to load menu entries for id ${id}`)
+  }
 }
