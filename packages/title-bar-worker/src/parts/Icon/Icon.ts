0b1e9913734c1b2d6640737b902c6ef156c29802
Le Vivilet <levivlet@pm.me>
2025-02-08 22:53:11 +0100

    feature: add icon module

---
+++ packages/title-bar-worker/src/parts/Icon/Icon.ts
@@ -0,0 +1,27 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const ExtensionDefaultIcon = `${AssetDir.assetDir}/icons/extensionDefaultIcon.png`
+export const ExtensionLanguageBasics = `${AssetDir.assetDir}/icons/language-icon.svg`
+export const ExtensionTheme = `${AssetDir.assetDir}/icons/theme-icon.png`
+export const ArrowDown = 'ArrowDown'
+export const Add = 'Add'
+export const ArrowUp = 'ArrowUp'
+export const Tag = 'Tag'
+export const Blank = 'Blank'
+export const ClearAll = 'ClearAll'
+export const Close = 'Close'
+export const Cloud = 'Cloud'
+export const CollapseAll = 'CollapseAll'
+export const DebugAlt2 = 'DebugAlt2'
+export const Ellipsis = 'Ellipsis'
+export const Edit = 'Edit'
+export const Extensions = 'Extensions'
+export const Files = 'Files'
+export const Refresh = 'Refresh'
+export const RecordKey = 'RecordKey'
+export const Search = 'Search'
+export const SettingsGear = 'SettingsGear'
+export const SourceControl = 'SourceControl'
+export const SplitHorizontal = 'SplitHorizontal'
+export const Trash = 'Trash'
+export const None = ''
