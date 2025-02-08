d6c1716d2dde6a5c8519579d14aff28dc2d1d9b0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-08 23:34:52 +0100

    feature: add getCommands function (#46)

---
+++ packages/title-bar-worker/src/parts/GetCommands/GetCommands.ts
@@ -0,0 +1,30 @@
+export const commandsIds = [
+  'closeMenu',
+  'focus',
+  'focusFirst',
+  'focusIndex',
+  'focusLast',
+  'focusNext',
+  'focusPrevious',
+  'handleKeyArrowDown',
+  'handleKeyArrowLeft',
+  'handleKeyArrowRight',
+  'handleKeyArrowUp',
+  'handleKeyEnd',
+  'handleKeyEnter',
+  'handleKeyEscape',
+  'handleKeyHome',
+  'handleKeySpace',
+  'handleMenuClick',
+  'handleMenuMouseOver',
+  'handleMouseOver',
+  'handleMouseOut',
+  'toggleIndex',
+  'toggleMenu',
+  'handleClick',
+  'handleFocus',
+]
+
+export const getCommanIds = (): readonly string[] => {
+  return commandsIds
+}
