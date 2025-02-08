b2b0c5a00d6ae7e368cc8ed2b7fb9320292578d2
Le Vivilet <levivlet@pm.me>
2025-02-08 23:02:57 +0100

    lint

--- packages/title-bar-worker/src/parts/TitleBarMenuBarStates/TitleBarMenuBarStates.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBarStates/TitleBarMenuBarStates.ts
@@ -1,5 +1,4 @@
-import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
-import { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as ViewletRegistry from '../ViewletRegistry/ViewletRegistry.ts'
 
 export const { get, set } = ViewletRegistry.create<TitleBarMenuBarState>()
