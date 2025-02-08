7054430354a06ed8800831ab9cdada32adbba2b9
Le Vivilet <levivlet@pm.me>
2025-02-08 22:57:14 +0100

    simplify icos

--- packages/title-bar-worker/src/parts/Icon/Icon.ts
+++ packages/title-bar-worker/src/parts/Icon/Icon.ts
@@ -1,8 +1,3 @@
-import * as AssetDir from '../AssetDir/AssetDir.js'
-
-export const ExtensionDefaultIcon = `${AssetDir.assetDir}/icons/extensionDefaultIcon.png`
-export const ExtensionLanguageBasics = `${AssetDir.assetDir}/icons/language-icon.svg`
-export const ExtensionTheme = `${AssetDir.assetDir}/icons/theme-icon.png`
 export const ArrowDown = 'ArrowDown'
 export const Add = 'Add'
 export const ArrowUp = 'ArrowUp'
