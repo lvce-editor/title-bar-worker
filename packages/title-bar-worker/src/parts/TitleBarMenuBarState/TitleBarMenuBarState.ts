5f29b946d0ef90037c8a01b82ae86287bfd42e1d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-08 22:40:10 +0100

    feature: add state interface (#9)

---
+++ packages/title-bar-worker/src/parts/TitleBarMenuBarState/TitleBarMenuBarState.ts
@@ -0,0 +1,17 @@
+export interface TitleBarMenuBarState {
+  readonly uid: number
+  readonly titleBarEntries: readonly any[]
+  readonly focusedIndex: number
+  readonly isMenuOpen: boolean
+  readonly menus: readonly any[]
+  readonly labelFontWeight: number
+  readonly labelFontFamily: string
+  readonly labelFontSize: number
+  readonly labelPadding: number
+  readonly labelLetterSpacing: number
+  readonly titleBarHeight: number
+  readonly x: number
+  readonly y: number
+  readonly width: number
+  readonly height: number
+}
