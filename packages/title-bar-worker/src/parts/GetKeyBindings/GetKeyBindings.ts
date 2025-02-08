ca9ace434236040c176a81c27a54c41a06481e95
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-08 23:34:34 +0100

    feature: update keybindings (#45)
    * feature: update keybindings
    
    * import
    
    * lint

--- packages/title-bar-worker/src/parts/GetKeyBindings/GetKeyBindings.ts
+++ packages/title-bar-worker/src/parts/GetKeyBindings/GetKeyBindings.ts
@@ -1,98 +1,47 @@
 import * as KeyCode from '../KeyCode/KeyCode.ts'
-import * as KeyModifier from '../KeyModifier/KeyModifier.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const getKeyBindings = (): readonly any[] => {
   return [
     {
-      key: KeyCode.RightArrow,
-      command: 'Explorer.handleArrowRight',
-      when: WhenExpression.FocusExplorer,
-    },
-    {
-      key: KeyCode.LeftArrow,
-      command: 'Explorer.handleArrowLeft',
-      when: WhenExpression.FocusExplorer,
-    },
-    {
-      key: KeyCode.Home,
-      command: 'Explorer.focusFirst',
-      when: WhenExpression.FocusExplorer,
-    },
-    {
-      key: KeyCode.End,
-      command: 'Explorer.focusLast',
-      when: WhenExpression.FocusExplorer,
+      key: KeyCode.DownArrow,
+      command: 'TitleBarMenuBar.handleKeyArrowDown',
+      when: WhenExpression.FocusTitleBarMenuBar,
     },
     {
       key: KeyCode.UpArrow,
-      command: 'Explorer.focusPrevious',
-      when: WhenExpression.FocusExplorer,
+      command: 'TitleBarMenuBar.handleKeyArrowUp',
+      when: WhenExpression.FocusTitleBarMenuBar,
     },
     {
-      key: KeyCode.DownArrow,
-      command: 'Explorer.focusNext',
-      when: WhenExpression.FocusExplorer,
+      key: KeyCode.RightArrow,
+      command: 'TitleBarMenuBar.handleKeyArrowRight',
+      when: WhenExpression.FocusTitleBarMenuBar,
     },
     {
-      key: KeyModifier.CtrlCmd | KeyCode.Star,
-      command: 'Explorer.expandAll',
-      when: WhenExpression.FocusExplorer,
-    },
-    {
-      key: KeyModifier.Alt | KeyCode.RightArrow,
-      command: 'Explorer.expandRecursively',
-      when: WhenExpression.FocusExplorer,
-    },
-    {
-      key: KeyModifier.CtrlCmd | KeyCode.LeftArrow,
-      command: 'Explorer.collapseAll',
-      when: WhenExpression.FocusExplorer,
-    },
-    {
-      key: KeyModifier.CtrlCmd | KeyCode.KeyV,
-      command: 'Explorer.handlePaste',
-      when: WhenExpression.FocusExplorer,
-    },
-    {
-      key: KeyModifier.CtrlCmd | KeyCode.KeyC,
-      command: 'Explorer.handleCopy',
-      when: WhenExpression.FocusExplorer,
-    },
-    {
-      key: KeyCode.F2,
-      command: 'Explorer.rename',
-      when: WhenExpression.FocusExplorer,
-    },
-    {
-      key: KeyCode.Escape,
-      command: 'Explorer.cancelEdit',
-      when: WhenExpression.FocusExplorerEditBox,
-    },
-    {
-      key: KeyCode.Enter,
-      command: 'Explorer.acceptEdit',
-      when: WhenExpression.FocusExplorerEditBox,
-    },
-    {
-      key: KeyCode.Delete,
-      command: 'Explorer.removeDirent',
-      when: WhenExpression.FocusExplorer,
-    },
-    {
-      key: KeyCode.Escape,
-      command: 'Explorer.focusNone',
-      when: WhenExpression.FocusExplorer,
+      key: KeyCode.LeftArrow,
+      command: 'TitleBarMenuBar.handleKeyArrowLeft',
+      when: WhenExpression.FocusTitleBarMenuBar,
     },
     {
       key: KeyCode.Space,
-      command: 'Explorer.handleClickCurrentButKeepFocus',
-      when: WhenExpression.FocusExplorer,
+      command: 'TitleBarMenuBar.handleKeySpace',
+      when: WhenExpression.FocusTitleBarMenuBar,
     },
     {
-      key: KeyCode.Enter,
-      command: 'Explorer.handleClickCurrent',
-      when: WhenExpression.FocusExplorer,
+      key: KeyCode.Home,
+      command: 'TitleBarMenuBar.handleKeyHome',
+      when: WhenExpression.FocusTitleBarMenuBar,
+    },
+    {
+      key: KeyCode.End,
+      command: 'TitleBarMenuBar.handleKeyEnd',
+      when: WhenExpression.FocusTitleBarMenuBar,
+    },
+    {
+      key: KeyCode.Escape,
+      command: 'TitleBarMenuBar.handleKeyEscape',
+      when: WhenExpression.FocusTitleBarMenuBar,
     },
   ]
 }
