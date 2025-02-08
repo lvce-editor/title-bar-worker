dc20cc7a1b6bee372db65af24247963fe0c6fbb5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-08 23:49:09 +0100

    feature: use titleBarEntries argument (#51)

--- packages/title-bar-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/title-bar-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,11 +1,8 @@
 import * as AddWidths from '../AddWidths/AddWidths.ts'
 
-export const loadContent = async (state: any): Promise<any> => {
+export const loadContent = async (state: any, titleBarEntries: readonly any[]): Promise<any> => {
   const { labelFontFamily, labelFontSize, labelFontWeight, labelLetterSpacing, labelPadding } = state
-  const titleBarEntries: any = []
   const withWidths = AddWidths.addWidths(titleBarEntries, labelPadding, labelFontWeight, labelFontSize, labelFontFamily, labelLetterSpacing)
-  // const visible = GetVisibleTitleBarEntries.getVisibleTitleBarEntries(withWidths, width)
-  // console.log({ visible })
   return {
     ...state,
     titleBarEntries: withWidths,
