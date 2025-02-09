bda8a7a86331e94b1b2476fc4fd6ab3d78c94f7c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-09 12:34:19 +0100

    feature: add getMenuEntries function (#75)
    * feature: add getMenuEntries function
    
    * import

--- packages/title-bar-worker/src/parts/GetMenuIds/GetMenuIds.ts
+++ packages/title-bar-worker/src/parts/GetMenuIds/GetMenuIds.ts
@@ -1,16 +1,35 @@
-import * as MenuEntryId from '../MenuEntryId/MenuEntryId.ts'
+import * as MenuEntriesEdit from '../MenuEntriesEdit/MenuEntriesEdit.ts'
+import * as MenuEntriesFile from '../MenuEntriesFile/MenuEntriesFile.ts'
+import * as MenuEntriesGo from '../MenuEntriesGo/MenuEntriesGo.ts'
+import * as MenuEntriesHelp from '../MenuEntriesHelp/MenuEntriesHelp.ts'
+import * as MenuEntriesOpenRecent from '../MenuEntriesOpenRecent/MenuEntriesOpenRecent.ts'
+import * as MenuEntriesRun from '../MenuEntriesRun/MenuEntriesRun.ts'
+import * as MenuEntriesSelection from '../MenuEntriesSelection/MenuEntriesSelection.ts'
+import * as MenuEntriesTerminal from '../MenuEntriesTerminal/MenuEntriesTerminal.ts'
+import * as MenuEntriesTitleBar from '../MenuEntriesTitleBar/MenuEntriesTitleBar.ts'
+import * as MenuEntriesView from '../MenuEntriesView/MenuEntriesView.ts'
+
+export const menus = [
+  MenuEntriesEdit,
+  MenuEntriesFile,
+  MenuEntriesGo,
+  MenuEntriesHelp,
+  MenuEntriesRun,
+  MenuEntriesSelection,
+  MenuEntriesTerminal,
+  MenuEntriesTitleBar,
+  MenuEntriesView,
+  MenuEntriesOpenRecent,
+]
 
 export const getMenuIds = (): readonly number[] => {
-  return [
-    MenuEntryId.Edit,
-    MenuEntryId.File,
-    MenuEntryId.Go,
-    MenuEntryId.Help,
-    MenuEntryId.Run,
-    MenuEntryId.Selection,
-    MenuEntryId.Terminal,
-    MenuEntryId.TitleBar,
-    MenuEntryId.View,
-    MenuEntryId.OpenRecent,
-  ]
+  return menus.map((menu) => menu.id)
+}
+
+export const getMenuEntries = (id: number): readonly any[] => {
+  const menu = menus.find((item) => item.id === id)
+  if (!menu) {
+    return []
+  }
+  return menu.getMenuEntries()
 }
