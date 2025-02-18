b53d75466eef660ebb4f09905115dba44ee86d22
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-18 19:27:09 +0100

    feature: move menus into separate file (#78)

--- packages/title-bar-worker/src/parts/GetMenuIds/GetMenuIds.ts
+++ packages/title-bar-worker/src/parts/GetMenuIds/GetMenuIds.ts
@@ -1,26 +1,4 @@
-import * as MenuEntriesEdit from '../MenuEntriesEdit/MenuEntriesEdit.ts'
-import * as MenuEntriesFile from '../MenuEntriesFile/MenuEntriesFile.ts'
-import * as MenuEntriesGo from '../MenuEntriesGo/MenuEntriesGo.ts'
-import * as MenuEntriesHelp from '../MenuEntriesHelp/MenuEntriesHelp.ts'
-import * as MenuEntriesOpenRecent from '../MenuEntriesOpenRecent/MenuEntriesOpenRecent.ts'
-import * as MenuEntriesRun from '../MenuEntriesRun/MenuEntriesRun.ts'
-import * as MenuEntriesSelection from '../MenuEntriesSelection/MenuEntriesSelection.ts'
-import * as MenuEntriesTerminal from '../MenuEntriesTerminal/MenuEntriesTerminal.ts'
-import * as MenuEntriesTitleBar from '../MenuEntriesTitleBar/MenuEntriesTitleBar.ts'
-import * as MenuEntriesView from '../MenuEntriesView/MenuEntriesView.ts'
-
-export const menus = [
-  MenuEntriesEdit,
-  MenuEntriesFile,
-  MenuEntriesGo,
-  MenuEntriesHelp,
-  MenuEntriesRun,
-  MenuEntriesSelection,
-  MenuEntriesTerminal,
-  MenuEntriesTitleBar,
-  MenuEntriesView,
-  MenuEntriesOpenRecent,
-]
+import { menus } from '../Menus/Menus.ts'
 
 export const getMenuIds = (): readonly number[] => {
   return menus.map((menu) => menu.id)
