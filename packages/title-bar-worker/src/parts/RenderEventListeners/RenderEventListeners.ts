05f32ac2f5973f5d10612256f1be95bfe539749f
Le Vivilet <levivlet@pm.me>
2025-03-04 22:21:59 +0100

    feature: add more event listeners

--- packages/title-bar-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
+++ packages/title-bar-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -14,5 +14,33 @@ export const renderEventListeners = (): readonly any[] => {
       name: DomEventListenerFunctions.HandleClickToggleMaximize,
       params: ['handleClickToggleMaximize'],
     },
+    {
+      name: 'handleFocusIn',
+      params: ['handlefocus'],
+    },
+    {
+      name: 'handleMenuClick',
+      params: ['handleMenuClick', 'event.clientX', 'event.clientY'],
+    },
+    {
+      name: 'handleMenuMouseOver',
+      params: ['handleMenuMouseOver', 'event.clientX', 'event.clientY'],
+    },
+    {
+      name: 'handleClick',
+      params: ['handleClick', 'event.button', 'event.clientX', 'event.clientY'],
+    },
+    {
+      name: 'handlePointerOut',
+      params: ['handlePointerOut', 'event.clientX', 'event.clientY'],
+    },
+    {
+      name: 'handlePointerOver',
+      params: ['handlePointerOver', 'event.clientX', 'event.clientY'],
+    },
+    {
+      name: 'handleFocusOut',
+      params: ['handleFocusOut', 'event.clientX', 'event.clientY'], // TODO maybe check relatedTarget
+    },
   ]
 }
