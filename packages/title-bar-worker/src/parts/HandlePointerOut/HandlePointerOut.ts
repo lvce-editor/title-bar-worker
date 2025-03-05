ce2b4b0e32f992b92f271b0bd9b597a871fe0da1
Le Vivilet <levivlet@pm.me>
2025-03-05 22:32:22 +0100

    feature: improve pointerOut function

--- packages/title-bar-worker/src/parts/HandlePointerOut/HandlePointerOut.ts
+++ packages/title-bar-worker/src/parts/HandlePointerOut/HandlePointerOut.ts
@@ -1,5 +1,11 @@
+import * as ViewletTitleBarMenuBarGetTitleBarIndexFromPosition from '../TitleBarMenuBar/ViewletTitleBarMenuBarGetTitleBarIndexFromPosition.ts'
+import * as ViewletTitleBarMenuBarHandleMouseOut from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleMouseOut.ts'
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 
-export const handlePointerOut = (state: TitleBarMenuBarState): TitleBarMenuBarState => {
-  return state
+export const handlePointerOut = (state: TitleBarMenuBarState, clientX: number, clientY: number): TitleBarMenuBarState => {
+  const index = ViewletTitleBarMenuBarGetTitleBarIndexFromPosition.getTitleBarIndexFromPosition(state.titleBarEntries, clientX - state.x, clientY)
+  if (index === -1) {
+    return state
+  }
+  return ViewletTitleBarMenuBarHandleMouseOut.handleMouseOut(state, index)
 }
