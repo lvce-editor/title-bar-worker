337ae7a4d4fe0dffcc9c7767018ac541b610edac
Le Vivilet <levivlet@pm.me>
2025-03-05 22:55:20 +0100

    feature: add ts memory config

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -3,6 +3,10 @@ import { root } from './root.ts'
 
 export const threshold = 500_000
 
+export const instantiations = 9000
+
+export const instantiationsPath = join(root, 'packages', 'title-bar-view')
+
 export const workerPath = join(root, '.tmp/dist/dist/titleBarWorkerMain.js')
 
 export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
--- packages/memory/src/measureMemory.ts
+++ packages/memory/src/measureMemory.ts
@@ -1,11 +1,13 @@
 import { measureMemory } from '@lvce-editor/measure-memory'
-import { playwrightPath, threshold, workerPath } from './config.ts'
+import { playwrightPath, threshold, workerPath, instantiations, instantiationsPath } from './config.ts'
 
 const main = async () => {
   await measureMemory({
     playwrightPath,
     workerPath,
     threshold,
+    instantiations,
+    instantiationsPath,
   })
 }
 
