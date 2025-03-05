6449814a794999826af5d53473e54585e387d734
Le Vivilet <levivlet@pm.me>
2025-03-05 22:22:16 +0100

    feature: call mouseoverIndex function

--- packages/title-bar-worker/src/parts/HandlePointerOver/HandlePointerOver.ts
+++ packages/title-bar-worker/src/parts/HandlePointerOver/HandlePointerOver.ts
@@ -1,5 +1,11 @@
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
+import * as ViewletTitleBarMenuBarGetTitleBarIndexFromPosition from '../TitleBarMenuBar/ViewletTitleBarMenuBarGetTitleBarIndexFromPosition.ts'
+import * as ViewletTitleBarMenuBarHandleMouseOver from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleMouseOver.ts'
 
-export const handlePointerOver = (state: TitleBarMenuBarState): TitleBarMenuBarState => {
-  return state
+export const handlePointerOver = (state: TitleBarMenuBarState, clientX: number, clientY: number): TitleBarMenuBarState => {
+  const index = ViewletTitleBarMenuBarGetTitleBarIndexFromPosition.getTitleBarIndexFromPosition(state.titleBarEntries, clientX - state.x, clientY)
+  if (index === -1) {
+    return state
+  }
+  return ViewletTitleBarMenuBarHandleMouseOver.handleMouseOver(state, index)
 }
