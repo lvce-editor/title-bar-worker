e305fc04e7118f836d5c5f3ebf74b3541da999b1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-05 22:20:29 +0100

    fix: typo (#87)

--- packages/title-bar-worker/src/parts/MeasureTitleBarEntryWidth/MeasureTitleBarEntryWidth.ts
+++ packages/title-bar-worker/src/parts/MeasureTitleBarEntryWidth/MeasureTitleBarEntryWidth.ts
@@ -1,7 +1,7 @@
-import * as MeasureTextWitdth from '../MeasureTextWidth/MeasureTextWidth.ts'
+import * as MeasureTextWidth from '../MeasureTextWidth/MeasureTextWidth.ts'
 
 export const measureTitleBarEntryWidth = (label: string, fontWeight: number, fontSize: number, fontFamily: string, letterSpacing: number): number => {
   const isMonospaceFont = false
   const charWidth = 0
-  return MeasureTextWitdth.measureTextWidth(label, fontWeight, fontSize, fontFamily, letterSpacing, isMonospaceFont, charWidth)
+  return MeasureTextWidth.measureTextWidth(label, fontWeight, fontSize, fontFamily, letterSpacing, isMonospaceFont, charWidth)
 }
