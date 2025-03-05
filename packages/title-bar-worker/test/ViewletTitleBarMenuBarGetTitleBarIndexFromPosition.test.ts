8e7575b9832e1f30a26f2db1f750e0a9632ca2b2
Le Vivilet <levivlet@pm.me>
2025-03-05 22:16:33 +0100

    feature: add getIndexFromPosition function

---
+++ packages/title-bar-worker/test/ViewletTitleBarMenuBarGetTitleBarIndexFromPosition.test.ts
@@ -0,0 +1,43 @@
+import { expect, test } from '@jest/globals'
+import * as ViewletTitleBarMenuBarGetTitleBarIndexFromPosition from '../src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarGetTitleBarIndexFromPosition.ts'
+
+test('getTitleBarIndexFromPosition - first item', () => {
+  const titleBarEntries = [
+    {
+      width: 40,
+    },
+    {
+      width: 40,
+    },
+  ]
+  expect(ViewletTitleBarMenuBarGetTitleBarIndexFromPosition.getTitleBarIndexFromPosition(titleBarEntries, 20, 10)).toBe(0)
+})
+
+test('getTitleBarIndexFromPosition - second item', () => {
+  const titleBarEntries = [
+    {
+      width: 40,
+    },
+    {
+      width: 40,
+    },
+  ]
+  expect(ViewletTitleBarMenuBarGetTitleBarIndexFromPosition.getTitleBarIndexFromPosition(titleBarEntries, 50, 10)).toBe(1)
+})
+
+test('getTitleBarIndexFromPosition - no match', () => {
+  const titleBarEntries = [
+    {
+      width: 40,
+    },
+    {
+      width: 40,
+    },
+  ]
+  expect(ViewletTitleBarMenuBarGetTitleBarIndexFromPosition.getTitleBarIndexFromPosition(titleBarEntries, 100, 10)).toBe(-1)
+})
+
+test('getTitleBarIndexFromPosition - empty entries', () => {
+  const titleBarEntries: any[] = []
+  expect(ViewletTitleBarMenuBarGetTitleBarIndexFromPosition.getTitleBarIndexFromPosition(titleBarEntries, 50, 10)).toBe(-1)
+})
