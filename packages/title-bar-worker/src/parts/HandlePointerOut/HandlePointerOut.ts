f28c350bece25a3b6a53e0432b9f9f4ea62b63c4
Le Vivilet <levivlet@pm.me>
2025-03-05 22:40:24 +0100

    lint

--- packages/title-bar-worker/src/parts/HandlePointerOut/HandlePointerOut.ts
+++ packages/title-bar-worker/src/parts/HandlePointerOut/HandlePointerOut.ts
@@ -1,6 +1,6 @@
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as ViewletTitleBarMenuBarGetTitleBarIndexFromPosition from '../TitleBarMenuBar/ViewletTitleBarMenuBarGetTitleBarIndexFromPosition.ts'
 import * as ViewletTitleBarMenuBarHandleMouseOut from '../TitleBarMenuBar/ViewletTitleBarMenuBarHandleMouseOut.ts'
-import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 
 export const handlePointerOut = (state: TitleBarMenuBarState, clientX: number, clientY: number): TitleBarMenuBarState => {
   const index = ViewletTitleBarMenuBarGetTitleBarIndexFromPosition.getTitleBarIndexFromPosition(state.titleBarEntries, clientX - state.x, clientY)
