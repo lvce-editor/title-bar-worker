c7351b8e71f42844a003564d86e66f7d337393c2
Le Vivilet <levivlet@pm.me>
2025-03-05 22:31:55 +0100

    feature: avoid focusIndex work when index doesn't change

--- packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarFocusIndex.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarFocusIndex.ts
@@ -5,10 +5,14 @@ import { openMenuAtIndex } from './ViewletTitleBarMenuBarOpenMenuAtIndex.ts'
 export const focusIndex = async (state: TitleBarMenuBarState, index: number): Promise<TitleBarMenuBarState> => {
   Assert.object(state)
   Assert.number(index)
-  const { isMenuOpen } = state
+  const { isMenuOpen, focusedIndex } = state
+  if (index === focusedIndex) {
+    return state
+  }
   if (isMenuOpen) {
     return openMenuAtIndex(state, index, /* focus */ false)
   }
+
   return {
     ...state,
     focusedIndex: index,
