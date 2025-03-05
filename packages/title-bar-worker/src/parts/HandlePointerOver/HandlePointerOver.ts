080c0a107ffd954f19d9108370fd91414c7f0b2f
Le Vivilet <levivlet@pm.me>
2025-03-05 22:05:59 +0100

    remove log

--- packages/title-bar-worker/src/parts/HandlePointerOver/HandlePointerOver.ts
+++ packages/title-bar-worker/src/parts/HandlePointerOver/HandlePointerOver.ts
@@ -1,6 +1,5 @@
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 
 export const handlePointerOver = (state: TitleBarMenuBarState): TitleBarMenuBarState => {
-  console.log('pointer over')
   return state
 }
