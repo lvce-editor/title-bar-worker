69eb5e5d337fb88dc3457ac177b95e2ac2c32576
Le Vivilet <levivlet@pm.me>
2025-03-07 18:56:44 +0100

    feature: simplify render function

--- packages/title-bar-worker/src/parts/DoRender/DoRender.ts
+++ packages/title-bar-worker/src/parts/DoRender/DoRender.ts
@@ -1,14 +1,8 @@
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
-import * as Render from '../Render/Render.ts'
-import * as TitleBarMenuBarStates from '../TitleBarMenuBarStates/TitleBarMenuBarStates.ts'
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
+import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
+import * as Diff from '../Diff/Diff.ts'
 
-export const doRender = async (uid: number): Promise<readonly VirtualDomNode[]> => {
-  const { oldState, newState } = TitleBarMenuBarStates.get(uid)
-  const commands = []
-  for (const item of Render.render) {
-    if (!item.isEqual(oldState, newState)) {
-      commands.push(item.apply(oldState, newState))
-    }
-  }
-  return commands
+export const doRender = (oldState: TitleBarMenuBarState, newState: TitleBarMenuBarState): any => {
+  const diffResult = Diff.diff(oldState, newState)
+  return ApplyRender.applyRender(oldState, newState, diffResult)
 }
