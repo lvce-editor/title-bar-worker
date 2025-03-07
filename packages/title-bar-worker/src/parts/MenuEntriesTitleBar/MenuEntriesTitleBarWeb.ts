ef121ae80a839fc17582a6e794dc21852618d770
Le Vivilet <levivlet@pm.me>
2025-03-07 19:32:48 +0100

    feature: add title bar strings tests

--- packages/title-bar-worker/src/parts/MenuEntriesTitleBar/MenuEntriesTitleBarWeb.ts
+++ packages/title-bar-worker/src/parts/MenuEntriesTitleBar/MenuEntriesTitleBarWeb.ts
@@ -1,6 +1,6 @@
 import * as MenuEntryId from '../MenuEntryId/MenuEntryId.ts'
 import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.ts'
-import * as ViewletTitleBarStrings from '../ViewletTitleBar/ViewletTitleBarStrings.ts'
+import * as ViewletTitleBarStrings from '../TitleBarStrings/TitleBarStrings.ts'
 
 export const getMenuEntries = (): any => {
   return [
