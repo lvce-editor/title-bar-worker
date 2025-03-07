fb77aa9fb472d3e36843c84f517ec3a70cebca4c
Le Vivilet <levivlet@pm.me>
2025-03-07 19:15:49 +0100

    sort

--- packages/title-bar-worker/src/parts/GetMenuItemSeparatorDom/GetMenuItemSeparatorDom.ts
+++ packages/title-bar-worker/src/parts/GetMenuItemSeparatorDom/GetMenuItemSeparatorDom.ts
@@ -1,7 +1,7 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 const separator: VirtualDomNode = {
   type: VirtualDomElements.Div,
