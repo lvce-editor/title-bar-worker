2ceca7c2cc0d6e95f02243d45be31593cc6f4b1f
Le Vivilet <levivlet@pm.me>
2025-03-07 18:42:03 +0100

    feature: simplify playwright path

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -9,4 +9,4 @@ export const instantiationsPath = join(root, 'packages', 'title-bar-worker')
 
 export const workerPath = join(root, '.tmp/dist/dist/titleBarWorkerMain.js')
 
-export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
+export const playwrightPath = import.meta.resolve('../../e2e/node_modules/playwright/index.mjs').toString()
