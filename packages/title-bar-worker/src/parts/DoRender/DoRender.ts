0db57fa282d02aba42a6209c932cc117dddabb90
Le Vivilet <levivlet@pm.me>
2025-03-07 19:41:36 +0100

    simplify code

--- packages/title-bar-worker/src/parts/DoRender/DoRender.ts
+++ packages/title-bar-worker/src/parts/DoRender/DoRender.ts
@@ -2,8 +2,9 @@ import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as Diff from '../Diff/Diff.ts'
 import * as TitleBarMenuBarStates from '../TitleBarMenuBarStates/TitleBarMenuBarStates.ts'
 
-export const doRender = (uid: number): any => {
+export const doRender = (uid: number): readonly any[] => {
   const { oldState, newState } = TitleBarMenuBarStates.get(uid)
   const diffResult = Diff.diff(oldState, newState)
-  return ApplyRender.applyRender(oldState, newState, diffResult)
+  const commands = ApplyRender.applyRender(oldState, newState, diffResult)
+  return commands
 }
