a745d88f787fd2a1fad9cbe2f1c7ffb5cdff5624
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-07 19:22:54 +0100

    feature: simplify menu items rendering (#108)

---
+++ packages/title-bar-worker/src/parts/GetMenuItemSubMenuDom/GetMenuItemSubMenuDom.ts
@@ -0,0 +1,34 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
+
+const arrowRight: VirtualDomNode = {
+  type: VirtualDomElements.Div,
+  className: ClassNames.MenuItemSubMenuArrowRight,
+  childCount: 0,
+}
+
+export const getMenuItemSubMenuDom = (menuItem: any): readonly VirtualDomNode[] => {
+  const { label, isFocused, isExpanded, level } = menuItem
+  let className = ClassNames.MenuItem
+  className += ' ' + ClassNames.MenuItemSubMenu
+  if (isFocused) {
+    className += ' ' + ClassNames.MenuItemFocused
+  }
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className,
+      role: AriaRoles.MenuItem,
+      tabIndex: -1,
+      ariaHasPopup: true,
+      ariaExpanded: isExpanded,
+      ariaOwns: isExpanded ? `Menu-${level + 1}` : undefined,
+      childCount: 2,
+    },
+    text(label),
+    arrowRight,
+  ]
+}
