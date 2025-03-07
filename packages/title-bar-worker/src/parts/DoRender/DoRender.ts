bed7e6338724559ef4f8ebdc0f8676df80a9a524
Le Vivilet <levivlet@pm.me>
2025-03-07 19:49:48 +0100

    use async

--- packages/title-bar-worker/src/parts/DoRender/DoRender.ts
+++ packages/title-bar-worker/src/parts/DoRender/DoRender.ts
@@ -2,7 +2,7 @@ import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as Diff from '../Diff/Diff.ts'
 import * as TitleBarMenuBarStates from '../TitleBarMenuBarStates/TitleBarMenuBarStates.ts'
 
-export const doRender = (uid: number): readonly any[] => {
+export const doRender = async (uid: number): Promise<readonly any[]> => {
   const { oldState, newState } = TitleBarMenuBarStates.get(uid)
   const diffResult = Diff.diff(oldState, newState)
   const commands = ApplyRender.applyRender(oldState, newState, diffResult)
