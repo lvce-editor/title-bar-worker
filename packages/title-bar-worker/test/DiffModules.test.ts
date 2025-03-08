435e21a3b5e30423afe31a6bba0181cfd0a4cf4e
Le Vivilet <levivlet@pm.me>
2025-03-08 10:12:45 +0100

    lint

--- packages/title-bar-worker/test/DiffModules.test.ts
+++ packages/title-bar-worker/test/DiffModules.test.ts
@@ -1,8 +1,8 @@
 import { expect, test } from '@jest/globals'
-import * as DiffModules from '../src/parts/DiffModules/DiffModules.ts'
 import * as DiffEntries from '../src/parts/DiffEntries/DiffEntries.ts'
 import * as DiffFocusedIndex from '../src/parts/DiffFocusedIndex/DiffFocusedIndex.ts'
 import * as DiffMenus from '../src/parts/DiffMenus/DiffMenus.ts'
+import * as DiffModules from '../src/parts/DiffModules/DiffModules.ts'
 
 test('modules - should export array with correct modules', () => {
   expect(DiffModules.modules).toEqual([DiffEntries, DiffFocusedIndex, DiffMenus])
