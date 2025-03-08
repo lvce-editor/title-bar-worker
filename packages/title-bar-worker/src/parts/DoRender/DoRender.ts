56594f7991b051ad8c1f72fb2e80980258687d3f
Le Vivilet <levivlet@pm.me>
2025-03-08 09:52:18 +0100

    lint

--- packages/title-bar-worker/src/parts/DoRender/DoRender.ts
+++ packages/title-bar-worker/src/parts/DoRender/DoRender.ts
@@ -1,7 +1,7 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as Diff from '../Diff/Diff.ts'
 import * as TitleBarMenuBarStates from '../TitleBarMenuBarStates/TitleBarMenuBarStates.ts'
-import { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 export const doRender = async (uid: number): Promise<readonly VirtualDomNode[]> => {
   const { oldState, newState } = TitleBarMenuBarStates.get(uid)
