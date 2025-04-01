b975eb4e0521178062ea1f3f997b81c2879ee5b9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-01 23:14:22 +0200

    feature: add diff2 command (#141)
    * feature: add diff2 command
    
    * add render2
    
    * coverage

---
+++ packages/title-bar-worker/src/parts/Render2/Render2.ts
@@ -0,0 +1,9 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
+import * as TitleBarMenuBarStates from '../TitleBarMenuBarStates/TitleBarMenuBarStates.ts'
+
+export const render2 = async (uid: number, diffResult: readonly number[]): Promise<readonly VirtualDomNode[]> => {
+  const { oldState, newState } = TitleBarMenuBarStates.get(uid)
+  const commands = ApplyRender.applyRender(oldState, newState, diffResult)
+  return commands
+}
