8b7b94084876e00c14f520e1f894738c25c111b0
Le Vivilet <levivlet@pm.me>
2025-04-01 23:11:56 +0200

    coverage

--- packages/title-bar-worker/src/parts/Render2/Render2.ts
+++ packages/title-bar-worker/src/parts/Render2/Render2.ts
@@ -1,6 +1,6 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as TitleBarMenuBarStates from '../TitleBarMenuBarStates/TitleBarMenuBarStates.ts'
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 export const render2 = async (uid: number, diffResult: readonly number[]): Promise<readonly VirtualDomNode[]> => {
   const { oldState, newState } = TitleBarMenuBarStates.get(uid)
