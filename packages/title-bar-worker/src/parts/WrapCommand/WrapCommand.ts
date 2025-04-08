b7a119683c85b0e4bb0f0157b2e3b5d03db40628
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-08 23:07:52 +0200

    feature: make wrap command function type-safe (#144)
    * feature: make wrap command function type-safe
    
    * coverage
    
    * skip test for now

--- packages/title-bar-worker/src/parts/WrapCommand/WrapCommand.ts
+++ packages/title-bar-worker/src/parts/WrapCommand/WrapCommand.ts
@@ -1,10 +1,23 @@
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as TitleBarMenuBarStates from '../TitleBarMenuBarStates/TitleBarMenuBarStates.ts'
 
-export const wrapCommand = (fn: any): any => {
+export interface WrappedFn {
+  (uid: number, ...args: readonly any[]): Promise<void>
+}
+
+interface Fn {
+  (state: TitleBarMenuBarState, ...args: readonly any[]): TitleBarMenuBarState | Promise<TitleBarMenuBarState>
+}
+
+export const wrapCommand = (fn: Fn): WrappedFn => {
   const wrapped = async (uid: number, ...args: readonly any[]): Promise<void> => {
     const { newState } = TitleBarMenuBarStates.get(uid)
     const newerState = await fn(newState, ...args)
-    TitleBarMenuBarStates.set(uid, newState, newerState)
+    if (newState === newerState) {
+      return
+    }
+    const latest = TitleBarMenuBarStates.get(uid)
+    TitleBarMenuBarStates.set(uid, latest.oldState, newerState)
   }
   return wrapped
 }
