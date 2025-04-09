17ecf4f6c590670da6d356475025deba890d813b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-09 21:31:52 +0200

    feature: add visibleMenuItem type (#157)
    * feature: add visibleMenuItem type
    
    * use helper

--- packages/title-bar-worker/src/parts/GetMenuItemVirtualDom/GetMenuItemVirtualDom.ts
+++ packages/title-bar-worker/src/parts/GetMenuItemVirtualDom/GetMenuItemVirtualDom.ts
@@ -1,7 +1,8 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import type { VisibleMenuItem } from '../VisibleMenuItem/VisibleMenuItem.ts'
 import * as GetMenuItemRenderer from '../GetMenuItemRenderer/GetMenuItemRenderer.ts'
 
-export const getMenuItemVirtualDom = (menuItem: any): readonly VirtualDomNode[] => {
+export const getMenuItemVirtualDom = (menuItem: VisibleMenuItem): readonly VirtualDomNode[] => {
   const { flags } = menuItem
   const fn = GetMenuItemRenderer.getMenuItemRenderer(flags)
   return fn(menuItem)
--- packages/title-bar-worker/src/parts/GetVisibleMenuItems/GetVisibleMenuItems.ts
+++ packages/title-bar-worker/src/parts/GetVisibleMenuItems/GetVisibleMenuItems.ts
@@ -1,5 +1,7 @@
-export const getVisible = (items: readonly any[], focusedIndex: number, expanded: boolean, level: number): readonly any[] => {
-  const visibleItems = []
+import type { VisibleMenuItem } from '../VisibleMenuItem/VisibleMenuItem.ts'
+
+export const getVisible = (items: readonly any[], focusedIndex: number, expanded: boolean, level: number): readonly VisibleMenuItem[] => {
+  const visibleItems: VisibleMenuItem[] = []
   for (let i = 0; i < items.length; i++) {
     const item = items[i]
     const { flags, label } = item
