2c27c21c1c131ade210537e6bfcd891640fc8e84
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-09 22:00:10 +0200

    feature: move text measure context into separate file (#163)

--- packages/title-bar-worker/src/parts/GetTextMeasureContext/GetTextMeasureContext.ts
+++ packages/title-bar-worker/src/parts/GetTextMeasureContext/GetTextMeasureContext.ts
@@ -1,15 +1,7 @@
+import * as CreateTextMeasureContext from '../CreateTextMeasureContext/CreateTextMeasureContext.ts'
 import * as MeasureTextWidthState from '../MeasureTextWidthState/MeasureTextWidthState.ts'
 
-const createCtx = (): OffscreenCanvasRenderingContext2D => {
-  const canvas = new OffscreenCanvas(0, 0)
-  const ctx = /** @type {OffscreenCanvasRenderingContext2D} */ canvas.getContext('2d')
-  if (!ctx) {
-    throw new Error('Failed to get canvas context 2d')
-  }
-  return ctx
-}
-
 export const getContext = (): OffscreenCanvasRenderingContext2D => {
-  const ctx = MeasureTextWidthState.getOrCreate(createCtx)
+  const ctx = MeasureTextWidthState.getOrCreate(CreateTextMeasureContext.createTextMeasureContext)
   return ctx
 }
