c7df131dc6396b31a2607c007a25dc78492facc2
Le Vivilet <levivlet@pm.me>
2025-04-09 22:06:36 +0200

    fix

--- packages/title-bar-worker/src/parts/ElectronWindow/ElectronWindow.ts
+++ packages/title-bar-worker/src/parts/ElectronWindow/ElectronWindow.ts
@@ -1,17 +1,17 @@
 import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 
 export const maximize = async (): Promise<void> => {
-  ParentRpc.invoke('ElectronWindow.unmaximize')
+  await ParentRpc.invoke('ElectronWindow.unmaximize')
 }
 
 export const unmaximize = async (): Promise<void> => {
-  ParentRpc.invoke('ElectronWindow.unmaximize')
+  await ParentRpc.invoke('ElectronWindow.unmaximize')
 }
 
 export const minimize = async (): Promise<void> => {
-  ParentRpc.invoke('ElectronWindow.minimize')
+  await ParentRpc.invoke('ElectronWindow.minimize')
 }
 
 export const close = async (): Promise<void> => {
-  ParentRpc.invoke('ElectronWindow.close')
+  await ParentRpc.invoke('ElectronWindow.close')
 }
