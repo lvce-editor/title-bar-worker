fd2f5dc513295953e3a52bf665808e1aadbca785
Le Vivilet <levivlet@pm.me>
2025-04-09 21:08:23 +0200

    feature: move focus function into separate file

--- packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleFocus.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleFocus.ts
@@ -1,7 +1,7 @@
+import * as Focus from '../Focus/Focus.ts'
 import * as FocusKey from '../FocusKey/FocusKey.ts'
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 
 export const handleFocus = async (state: any): Promise<any> => {
-  await ParentRpc.invoke('Focus.setFocus', FocusKey.TitleBarMenuBar)
+  await Focus.setFocus(FocusKey.TitleBarMenuBar)
   return state
 }
