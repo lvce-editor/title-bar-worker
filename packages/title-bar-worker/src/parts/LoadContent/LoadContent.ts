503d1974ddb9906a90a93e080daea73c309f162b
Le Vivilet <levivlet@pm.me>
2025-04-09 21:58:50 +0200

    feature: use state type

--- packages/title-bar-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/title-bar-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,6 +1,7 @@
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as AddWidths from '../AddWidths/AddWidths.ts'
 
-export const loadContent = async (state: any, titleBarEntries: readonly any[]): Promise<any> => {
+export const loadContent = async (state: TitleBarMenuBarState, titleBarEntries: readonly any[]): Promise<TitleBarMenuBarState> => {
   const { labelFontFamily, labelFontSize, labelFontWeight, labelLetterSpacing, labelPadding } = state
   const withWidths = AddWidths.addWidths(titleBarEntries, labelPadding, labelFontWeight, labelFontSize, labelFontFamily, labelLetterSpacing)
   return {
