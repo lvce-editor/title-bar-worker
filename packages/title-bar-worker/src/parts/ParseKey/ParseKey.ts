03219a5018507aee256747791c55ac11da5324fa
Le Vivilet <levivlet@pm.me>
2025-09-01 12:15:22 +0200

    feature: use keymodifier from virtual dom worker

--- packages/title-bar-worker/src/parts/ParseKey/ParseKey.ts
+++ packages/title-bar-worker/src/parts/ParseKey/ParseKey.ts
@@ -1,7 +1,7 @@
 import type { ParsedKey } from '../ParsedKey/ParsedKey.ts'
 import * as Assert from '../Assert/Assert.ts'
 import * as GetKeyCodeString from '../GetKeyCodeString/GetKeyCodeString.ts'
-import * as KeyModifier from '../KeyModifier/KeyModifier.ts'
+import { KeyModifier } from '@lvce-editor/virtual-dom-worker'
 
 export const parseKey = (rawKey: number): ParsedKey => {
   Assert.number(rawKey)
