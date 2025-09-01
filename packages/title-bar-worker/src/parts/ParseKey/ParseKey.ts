bec84d814d16d8d8d43b2e9dc5626b2d4afa2d32
Le Vivilet <levivlet@pm.me>
2025-09-01 12:17:50 +0200

    lint

--- packages/title-bar-worker/src/parts/ParseKey/ParseKey.ts
+++ packages/title-bar-worker/src/parts/ParseKey/ParseKey.ts
@@ -1,7 +1,7 @@
+import { KeyModifier } from '@lvce-editor/virtual-dom-worker'
 import type { ParsedKey } from '../ParsedKey/ParsedKey.ts'
 import * as Assert from '../Assert/Assert.ts'
 import * as GetKeyCodeString from '../GetKeyCodeString/GetKeyCodeString.ts'
-import { KeyModifier } from '@lvce-editor/virtual-dom-worker'
 
 export const parseKey = (rawKey: number): ParsedKey => {
   Assert.number(rawKey)
