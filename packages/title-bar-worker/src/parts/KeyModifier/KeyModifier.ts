24d60040d93ca8a0f9339a859926a93cf8f75a60
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-01 12:24:45 +0200

    feature: use keymodifier from virtual dom worker (#192)
    * feature: use keymodifier from virtual dom worker
    
    * lint
    
    * remove unused

--- packages/title-bar-worker/src/parts/KeyModifier/KeyModifier.ts
+++
@@ -1,2 +0,0 @@
-export const CtrlCmd = (1 << 11) >>> 0
-export const Shift = (1 << 10) >>> 0
--- packages/title-bar-worker/src/parts/ParseKey/ParseKey.ts
+++ packages/title-bar-worker/src/parts/ParseKey/ParseKey.ts
@@ -1,7 +1,7 @@
+import { KeyModifier } from '@lvce-editor/virtual-dom-worker'
 import type { ParsedKey } from '../ParsedKey/ParsedKey.ts'
 import * as Assert from '../Assert/Assert.ts'
 import * as GetKeyCodeString from '../GetKeyCodeString/GetKeyCodeString.ts'
-import * as KeyModifier from '../KeyModifier/KeyModifier.ts'
 
 export const parseKey = (rawKey: number): ParsedKey => {
   Assert.number(rawKey)
