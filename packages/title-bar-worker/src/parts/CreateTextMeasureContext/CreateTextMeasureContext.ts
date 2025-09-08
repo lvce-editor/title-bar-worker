fe139826ab8a0cc53760dafa388116617c4ae015
Le Vivilet <levivilet@pm.me>
2025-09-08 11:25:25 +0200

    feature: make text width measurement more functional (#196)
    * feature: make text width measurement more functional
    
    * lint

--- packages/title-bar-worker/src/parts/CreateTextMeasureContext/CreateTextMeasureContext.ts
+++ packages/title-bar-worker/src/parts/CreateTextMeasureContext/CreateTextMeasureContext.ts
@@ -1,8 +1,10 @@
-export const createTextMeasureContext = (): OffscreenCanvasRenderingContext2D => {
+export const createTextMeasureContext = (letterSpacing: string, font: string): OffscreenCanvasRenderingContext2D => {
   const canvas = new OffscreenCanvas(0, 0)
   const ctx = canvas.getContext('2d')
   if (!ctx) {
     throw new Error('Failed to get canvas context 2d')
   }
+  ctx.letterSpacing = letterSpacing
+  ctx.font = font
   return ctx
 }
--- packages/title-bar-worker/src/parts/MeasureTitleBarEntryWidth/MeasureTitleBarEntryWidth.ts
+++
@@ -1,7 +0,0 @@
-import * as MeasureTextWidth from '../MeasureTextWidth/MeasureTextWidth.ts'
-
-export const measureTitleBarEntryWidth = (label: string, fontWeight: number, fontSize: number, fontFamily: string, letterSpacing: number): number => {
-  const isMonospaceFont = false
-  const charWidth = 0
-  return MeasureTextWidth.measureTextWidth(label, fontWeight, fontSize, fontFamily, letterSpacing, isMonospaceFont, charWidth)
-}
