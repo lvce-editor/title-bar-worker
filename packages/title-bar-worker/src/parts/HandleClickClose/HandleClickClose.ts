0aa7a333f34525b9d64f848a7702592f43bfb63c
Le Vivilet <levivilet@pm.me>
2025-10-01 22:15:57 +0200

    feature: add click types (#214)

--- packages/title-bar-worker/src/parts/HandleClickClose/HandleClickClose.ts
+++ packages/title-bar-worker/src/parts/HandleClickClose/HandleClickClose.ts
@@ -1,6 +1,7 @@
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as ElectronWindow from '../ElectronWindow/ElectronWindow.ts'
 
-export const handleClickClose = async (state: any): Promise<any> => {
+export const handleClickClose = async (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
   await ElectronWindow.close()
   return state
 }
