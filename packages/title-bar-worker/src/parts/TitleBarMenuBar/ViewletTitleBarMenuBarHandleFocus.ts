d83776db0733771c600bbe50ce6f2fc64c4b6f10
Le Vivilet <levivilet@pm.me>
2025-10-01 22:10:07 +0200

    feature: add focus type

--- packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleFocus.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleFocus.ts
@@ -1,7 +1,8 @@
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as Focus from '../Focus/Focus.ts'
 import * as FocusKey from '../FocusKey/FocusKey.ts'
 
-export const handleFocus = async (state: any): Promise<any> => {
+export const handleFocus = async (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
   await Focus.setFocus(FocusKey.TitleBarMenuBar)
   return state
 }
