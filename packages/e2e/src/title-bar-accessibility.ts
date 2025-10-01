d51818b397afdfab7bbc07375e8672f91d382013
Le Vivilet <levivilet@pm.me>
2025-10-01 22:50:26 +0200

    tsc

--- packages/e2e/src/title-bar-accessibility.ts
+++ packages/e2e/src/title-bar-accessibility.ts
@@ -41,6 +41,7 @@ export const test: Test = async ({ FileSystem, Locator, expect, Workspace }) => 
   await expect(menuItemFile).toHaveAttribute('aria-haspopup', 'true')
   await expect(menuItemFile).toHaveAttribute('aria-expanded', 'false')
   await expect(menuItemFile).toHaveAttribute('role', 'menuitem')
+  // @ts-ignore
   await menuItemFile.click()
 
   await expect(menuItemFile).toHaveAttribute('aria-expanded', 'true')
