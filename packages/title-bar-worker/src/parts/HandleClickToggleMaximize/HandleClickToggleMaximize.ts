95c5bf2015e8df65efb22e5a77c97d4eaa58c654
Le Vivilet <levivilet@pm.me>
2025-10-01 22:14:39 +0200

    feature: add click types

--- packages/title-bar-worker/src/parts/HandleClickToggleMaximize/HandleClickToggleMaximize.ts
+++ packages/title-bar-worker/src/parts/HandleClickToggleMaximize/HandleClickToggleMaximize.ts
@@ -1,7 +1,9 @@
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as ElectronWindow from '../ElectronWindow/ElectronWindow.ts'
 import * as NativeHostState from '../NativeHostState/NativeHostState.ts'
 
-export const handleClickToggleMaximize = async (state: any): Promise<any> => {
+export const handleClickToggleMaximize = async (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
+  // TODO merge nativeHostState with state
   const isMaximized = NativeHostState.isMaximized()
   const fn = isMaximized ? ElectronWindow.unmaximize : ElectronWindow.maximize
   await fn()
