e201cf42f612c65d8bdfe2c23625bdd9d69443ee
Le Vivilet <levivilet@pm.me>
2025-10-01 22:17:05 +0200

    feature: add focus types

--- packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarFocusLast.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarFocusLast.ts
@@ -1,7 +1,8 @@
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as ListIndex from '../ListIndex/ListIndex.ts'
 import { focusIndex } from './ViewletTitleBarMenuBarFocusIndex.ts'
 
-export const focusLast = (state: any): any => {
+export const focusLast = (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
   const { titleBarEntries } = state
   const indexToFocus = ListIndex.last(titleBarEntries)
   return focusIndex(state, indexToFocus)
