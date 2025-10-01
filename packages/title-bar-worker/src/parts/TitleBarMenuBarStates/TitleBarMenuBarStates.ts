955fd6e7f6e028c2eb99d5051b065ef27eaf666d
Le Vivilet <levivilet@pm.me>
2025-10-01 22:00:03 +0200

    lint

--- packages/title-bar-worker/src/parts/TitleBarMenuBarStates/TitleBarMenuBarStates.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBarStates/TitleBarMenuBarStates.ts
@@ -1,4 +1,4 @@
-import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 
 export const { get, set, getCommandIds, registerCommands } = ViewletRegistry.create<TitleBarMenuBarState>()
