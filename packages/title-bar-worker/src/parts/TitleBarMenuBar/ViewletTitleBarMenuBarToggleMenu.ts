f19e29dd33391a70686a70f7ba873b02d6ce445a
Le Vivilet <levivilet@pm.me>
2025-10-01 22:26:24 +0200

    feature: add toggle types (#220)

--- packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarToggleMenu.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarToggleMenu.ts
@@ -1,7 +1,8 @@
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import { closeMenu } from './ViewletTitleBarMenuBarCloseMenu.ts'
 import { openMenu } from './ViewletTitleBarMenuBarOpenMenu.ts'
 
-export const toggleMenu = (state: any): any => {
+export const toggleMenu = async (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
   const { isMenuOpen } = state
   if (isMenuOpen) {
     return closeMenu(state, /* keepFocus */ true)
