2267b26ffe1e7be9e93931fa17c0e9372c3a21f0
Le Vivilet <levivilet@pm.me>
2025-10-01 22:26:05 +0200

    feature: add closeMenu types

--- packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleMouseOverMenuClosed.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleMouseOverMenuClosed.ts
@@ -1,5 +1,6 @@
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import { focusIndex } from './ViewletTitleBarMenuBarFocusIndex.ts'
 
-export const handleMouseOverMenuClosed = (state: any, index: number): any => {
+export const handleMouseOverMenuClosed = (state: TitleBarMenuBarState, index: number): Promise<TitleBarMenuBarState> => {
   return focusIndex(state, index)
 }
