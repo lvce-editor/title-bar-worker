71682cc8efff59047f44f8d3538ec0948b814870
Le Vivilet <levivilet@pm.me>
2025-10-01 21:53:37 +0200

    feature: simplify viewlet registry

--- packages/title-bar-worker/src/parts/TitleBarMenuBarStates/TitleBarMenuBarStates.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBarStates/TitleBarMenuBarStates.ts
@@ -1,4 +1,4 @@
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
-import * as ViewletRegistry from '../ViewletRegistry/ViewletRegistry.ts'
+import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
 
-export const { get, set } = ViewletRegistry.create<TitleBarMenuBarState>()
+export const { get, set, getCommandIds, registerCommands } = ViewletRegistry.create<TitleBarMenuBarState>()
