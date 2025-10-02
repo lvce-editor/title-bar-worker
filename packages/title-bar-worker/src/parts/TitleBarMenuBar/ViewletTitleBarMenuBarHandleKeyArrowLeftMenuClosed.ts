3a433cf977fe0598a17dfce8839e7c48966823c5
Le Vivilet <levivilet@pm.me>
2025-10-02 17:51:36 +0200

    tsc

--- packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleKeyArrowLeftMenuClosed.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleKeyArrowLeftMenuClosed.ts
@@ -1,7 +1,7 @@
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import { focusPrevious } from './ViewletTitleBarMenuBarFocusPrevious.ts'
 
-export const handleKeyArrowLeftMenuClosed = (state: TitleBarMenuBarState): TitleBarMenuBarState => {
+export const handleKeyArrowLeftMenuClosed = (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
   // TODO menu collapse
   return focusPrevious(state)
 }
