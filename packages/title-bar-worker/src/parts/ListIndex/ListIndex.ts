83f50fab4c021d5142452cdde4751c65af5abc44
Le Vivilet <levivilet@pm.me>
2025-10-02 17:18:52 +0200

    feature: use types for list

--- packages/title-bar-worker/src/parts/ListIndex/ListIndex.ts
+++ packages/title-bar-worker/src/parts/ListIndex/ListIndex.ts
@@ -2,14 +2,14 @@ export const first = (): number => {
   return 0
 }
 
-export const last = (items: any): number => {
+export const last = <T>(items: readonly T[]): number => {
   return items.length - 1
 }
 
-export const next = (items: any, index: number): number => {
+export const next = <T>(items: readonly T[], index: number): number => {
   return (index + 1) % items.length
 }
 
-export const previous = (items: any, index: number): number => {
+export const previous = <T>(items: readonly T[], index: number): number => {
   return index === 0 ? items.length - 1 : index - 1
 }
