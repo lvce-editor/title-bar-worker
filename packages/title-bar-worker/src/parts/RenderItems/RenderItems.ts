bceefbbf037b786a8a4c58b668f6b6d51a7c74fc
Le Vivilet <levivilet@pm.me>
2025-10-14 13:41:55 +0200

    feature: render title dom (#274)

--- packages/title-bar-worker/src/parts/RenderItems/RenderItems.ts
+++ packages/title-bar-worker/src/parts/RenderItems/RenderItems.ts
@@ -1,11 +1,18 @@
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import { getIcon } from '../GetIcon/GetIcon.ts'
+import { getTitleBarIconVirtualDom } from '../GetTitleBarIconVirtualDom/GetTitleBarIconVirtualDom.ts'
 import * as GetTitleBarMenuBarVirtualDom from '../GetTitleBarMenuBarVirtualDom/GetTitleBarMenuBarVirtualDom.ts'
+import { getTitleVirtualDom } from '../GetTitleVirtualDom/GetTitleVirtualDom.ts'
 import { getVisibleTitleBarEntries } from '../GetVisibleTitleBarEntries/GetVisibleTitleBarEntries.ts'
 
 export const renderItems = (state: TitleBarMenuBarState): readonly VirtualDomNode[] => {
-  const { titleBarEntries, width, focusedIndex, isMenuOpen } = state
+  const { titleBarEntries, width, focusedIndex, isMenuOpen, assetDir, title } = state
   const visibleEntries = getVisibleTitleBarEntries(titleBarEntries, width, focusedIndex, isMenuOpen)
-  const dom = GetTitleBarMenuBarVirtualDom.getTitleBarMenuBarVirtualDom(visibleEntries, focusedIndex)
+  const iconSrc = getIcon(assetDir)
+  const iconDom = getTitleBarIconVirtualDom(iconSrc)
+  const menuBarDom = GetTitleBarMenuBarVirtualDom.getTitleBarMenuBarVirtualDom(visibleEntries, focusedIndex)
+  const titleDom = getTitleVirtualDom(title)
+  const dom = [...iconDom, ...menuBarDom, ...titleDom]
   return dom
 }
