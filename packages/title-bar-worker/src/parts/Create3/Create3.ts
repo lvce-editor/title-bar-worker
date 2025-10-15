90ca300cf2c9d5b721380c01db5e278c21161f2e
Le Vivilet <levivilet@pm.me>
2025-10-15 14:15:39 +0200

    feature: add new create function (#282)

---
+++ packages/title-bar-worker/src/parts/Create3/Create3.ts
@@ -0,0 +1,44 @@
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
+import { createDefaultState } from '../CreateDefaultState/CreateDefaultState.ts'
+import * as GetTitleBarButtons from '../GetTitleBarButtons/GetTitleBarButtons.ts'
+import * as TitleBarMenuBarStates from '../TitleBarMenuBarStates/TitleBarMenuBarStates.ts'
+
+export const create3 = (
+  id: any,
+  uri: string,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  platform: number,
+  controlsOverlayEnabled: boolean,
+  titleBarStyleCustom: boolean,
+): TitleBarMenuBarState => {
+  const titleBarButtons = GetTitleBarButtons.getTitleBarButtons(platform, controlsOverlayEnabled, titleBarStyleCustom)
+  
+  const state: TitleBarMenuBarState = {
+    ...createDefaultState(),
+    uid: id,
+    titleBarEntries: [],
+    focusedIndex: -1,
+    isMenuOpen: false,
+    menus: [],
+    labelFontWeight: 400,
+    labelFontSize: 13,
+    labelFontFamily: 'system-ui, Ubuntu, Droid Sans, sans-serif',
+    labelPadding: 8,
+    labelLetterSpacing: 0,
+    titleBarHeight: height,
+    x,
+    y,
+    width,
+    height,
+    iconWidth: 30,
+    platform,
+    controlsOverlayEnabled,
+    titleBarStyleCustom,
+    titleBarButtons,
+  }
+  TitleBarMenuBarStates.set(id, state, state)
+  return state
+}
