65b857d17e0df19e05915dfdc7787b1631a466d5
Le Vivilet <levivilet@pm.me>
2025-10-15 14:14:29 +0200

    feature: add new properties (#280)

--- packages/title-bar-worker/src/parts/TitleBarMenuBarState/TitleBarMenuBarState.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBarState/TitleBarMenuBarState.ts
@@ -3,24 +3,28 @@ import type { TitleBarButton } from '../TitleBarButton/TitleBarButton.ts'
 export interface TitleBarMenuBarState {
   readonly assetDir: string
   readonly buttons: readonly TitleBarButton[]
+  readonly controlsOverlayEnabled: boolean
   readonly focusedIndex: number
   readonly height: number
   readonly iconWidth: number
   readonly isMenuOpen: boolean
-  readonly itleBarIconEnabled: boolean
+  readonly titleBarIconEnabled: boolean
   readonly labelFontFamily: string
   readonly labelFontSize: number
   readonly labelFontWeight: number
   readonly labelLetterSpacing: number
   readonly labelPadding: number
   readonly menus: readonly any[]
+  readonly platform: number
   readonly title: string
+  readonly titleBarButtons: readonly TitleBarButton[]
   readonly titleBarButtonsEnabled: boolean
   readonly titleBarButtonsWidth: number
   readonly titleBarEntries: readonly any[]
   readonly titleBarHeight: number
   readonly titleBarIconWidth: number
   readonly titleBarMenuBarEnabled: boolean
+  readonly titleBarStyleCustom: boolean
   readonly titleBarTitleEnabled: boolean
   readonly uid: number
   readonly width: number
