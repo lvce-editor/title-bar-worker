288b7de9c112ceafcabd0af38e7190fcbe2ece69
Le Vivilet <levivilet@pm.me>
2025-10-16 15:54:34 +0200

    feature: update window rpc mocking (#321)

--- packages/title-bar-worker/test/ElectronWindow.test.ts
+++ packages/title-bar-worker/test/ElectronWindow.test.ts
@@ -1,64 +1,35 @@
 import { expect, test } from '@jest/globals'
-import { MockRpc } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as ElectronWindow from '../src/parts/ElectronWindow/ElectronWindow.ts'
 
 test('maximize', async () => {
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: (method: string) => {
-      if (method === 'ElectronWindow.maximize') {
-        return undefined
-      }
-      throw new Error(`unexpected method ${method}`)
-    },
+  const mockRpc = RendererWorker.registerMockRpc({
+    'ElectronWindow.maximize'() {},
   })
-  RendererWorker.set(mockRpc)
-
   await expect(ElectronWindow.maximize()).resolves.toBeUndefined()
+  expect(mockRpc.invocations).toEqual([['ElectronWindow.maximize']])
 })
 
 test('unmaximize', async () => {
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: (method: string) => {
-      if (method === 'ElectronWindow.unmaximize') {
-        return undefined
-      }
-      throw new Error(`unexpected method ${method}`)
-    },
+  const mockRpc = RendererWorker.registerMockRpc({
+    'ElectronWindow.unmaximize'() {},
   })
-  RendererWorker.set(mockRpc)
-
   await expect(ElectronWindow.unmaximize()).resolves.toBeUndefined()
+  expect(mockRpc.invocations).toEqual([['ElectronWindow.unmaximize']])
 })
 
 test('minimize', async () => {
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: (method: string) => {
-      if (method === 'ElectronWindow.minimize') {
-        return undefined
-      }
-      throw new Error(`unexpected method ${method}`)
-    },
+  const mockRpc = RendererWorker.registerMockRpc({
+    'ElectronWindow.minimize'() {},
   })
-  RendererWorker.set(mockRpc)
-
   await expect(ElectronWindow.minimize()).resolves.toBeUndefined()
+  expect(mockRpc.invocations).toEqual([['ElectronWindow.minimize']])
 })
 
 test('close', async () => {
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: (method: string) => {
-      if (method === 'ElectronWindow.close') {
-        return undefined
-      }
-      throw new Error(`unexpected method ${method}`)
-    },
+  const mockRpc = RendererWorker.registerMockRpc({
+    'ElectronWindow.close'() {},
   })
-  RendererWorker.set(mockRpc)
-
   await expect(ElectronWindow.close()).resolves.toBeUndefined()
+  expect(mockRpc.invocations).toEqual([['ElectronWindow.close']])
 })
