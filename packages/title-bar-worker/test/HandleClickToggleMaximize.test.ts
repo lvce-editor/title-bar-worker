08a78d44bbacda210ffc73dcad913200c06ae727
Le Vivilet <levivilet@pm.me>
2025-10-16 17:49:39 +0200

    mock

--- packages/title-bar-worker/test/HandleClickToggleMaximize.test.ts
+++ packages/title-bar-worker/test/HandleClickToggleMaximize.test.ts
@@ -2,11 +2,15 @@ import { expect, test } from '@jest/globals'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as HandleClickToggleMaximize from '../src/parts/HandleClickToggleMaximize/HandleClickToggleMaximize.ts'
+import * as NativeHostState from '../src/parts/NativeHostState/NativeHostState.ts'
 
 test('handleClickToggleMaximize - calls maximize when not maximized', async () => {
   const mockRpc = RendererWorker.registerMockRpc({
     'ElectronWindow.maximize'() {},
   })
+
+  // Set the maximized state to false
+  NativeHostState.setMaximized(false)
 
   const state = { ...createDefaultState(), height: 600 }
   const result = await HandleClickToggleMaximize.handleClickToggleMaximize(state)
@@ -19,6 +23,9 @@ test('handleClickToggleMaximize - calls unmaximize when maximized', async () => 
     'ElectronWindow.unmaximize'() {},
     'ElectronWindow.maximize'() {},
   })
+
+  // Set the maximized state to true
+  NativeHostState.setMaximized(true)
 
   const state = { ...createDefaultState(), height: 600, isMaximized: true }
   const result = await HandleClickToggleMaximize.handleClickToggleMaximize(state)
