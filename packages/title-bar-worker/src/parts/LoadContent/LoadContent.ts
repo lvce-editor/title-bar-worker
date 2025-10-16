9a8890ac6e389708839790f961d84c3a36455f7e
Le Vivilet <levivilet@pm.me>
2025-10-16 08:26:04 +0200

    feature: load buttons in loadContent

--- packages/title-bar-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/title-bar-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,12 +1,17 @@
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import type { VisibleMenuItem } from '../VisibleMenuItem/VisibleMenuItem.ts'
 import * as AddWidths from '../AddWidths/AddWidths.ts'
+import { getTitleBarButtons } from '../GetTitleBarButtons/GetTitleBarButtons.ts'
 
 export const loadContent = async (state: TitleBarMenuBarState, titleBarEntries: readonly VisibleMenuItem[]): Promise<TitleBarMenuBarState> => {
-  const { labelFontFamily, labelFontSize, labelFontWeight, labelLetterSpacing, labelPadding } = state
+  const {
+    platform, controlsOverlayEnabled, titleBarStyleCustom,
+    labelFontFamily, labelFontSize, labelFontWeight, labelLetterSpacing, labelPadding } = state
   const withWidths = AddWidths.addWidths(titleBarEntries, labelPadding, labelFontWeight, labelFontSize, labelFontFamily, labelLetterSpacing)
+  const buttons = getTitleBarButtons(platform, controlsOverlayEnabled, titleBarStyleCustom)
   return {
     ...state,
     titleBarEntries: withWidths,
+    buttons
   }
 }
