2fed073000041a1e87981cef430bfb609f051ab5
Le Vivilet <levivilet@pm.me>
2025-10-17 10:00:10 +0200

    feature: load workspace uri (#336)
    * feature: load workspace uri
    
    * memory

--- packages/title-bar-worker/src/parts/LoadContent2/LoadContent2.ts
+++ packages/title-bar-worker/src/parts/LoadContent2/LoadContent2.ts
@@ -3,6 +3,7 @@ import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuB
 import * as AddWidths from '../AddWidths/AddWidths.ts'
 import { getTitleBarButtons } from '../GetTitleBarButtons/GetTitleBarButtons.ts'
 import * as MenuEntries from '../MenuEntries/MenuEntries.ts'
+import * as RendererWorker from '../ParentRpc/ParentRpc.ts'
 
 export const loadContent2 = async (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
   const { platform, controlsOverlayEnabled, titleBarStyleCustom, labelFontFamily, labelFontSize, labelFontWeight, labelLetterSpacing, labelPadding } =
@@ -10,6 +11,8 @@ export const loadContent2 = async (state: TitleBarMenuBarState): Promise<TitleBa
   const titleBarEntries = await MenuEntries.getMenuEntries(MenuEntryId.TitleBar)
   const withWidths = AddWidths.addWidths(titleBarEntries, labelPadding, labelFontWeight, labelFontSize, labelFontFamily, labelLetterSpacing)
   const buttons = getTitleBarButtons(platform, controlsOverlayEnabled, titleBarStyleCustom)
+  // @ts-ignore
+  const workspaceUri = await RendererWorker.invoke('Workspace.getUri')
   const title = 'test'
   const iconWidth = 30
   return {
