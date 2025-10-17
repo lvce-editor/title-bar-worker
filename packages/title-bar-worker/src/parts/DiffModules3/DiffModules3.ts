e91820eabe992f965c3ba44c88affca8cca3d169
Le Vivilet <levivilet@pm.me>
2025-10-17 08:01:16 +0200

    feature: simplify diff

--- packages/title-bar-worker/src/parts/DiffModules3/DiffModules3.ts
+++ packages/title-bar-worker/src/parts/DiffModules3/DiffModules3.ts
@@ -1,7 +1,8 @@
 import * as DiffFocusedIndex from '../DiffFocusedIndex/DiffFocusedIndex.ts'
 import * as DiffMenus from '../DiffMenus/DiffMenus.ts'
 import * as DiffTitleBar from '../DiffTitleBar/DiffTitleBar.ts'
+import * as DiffType from '../DiffType/DiffType.ts'
 
 export const modules = [DiffTitleBar.isEqual, DiffFocusedIndex.isEqual, DiffMenus.isEqual]
 
-export const numbers = [DiffTitleBar.diffType, DiffFocusedIndex.diffType, DiffMenus.diffType]
+export const numbers = [DiffType.RenderEntries, DiffType.RenderFocusedIndex, DiffType.RenderMenus]
