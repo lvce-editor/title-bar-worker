46e3aef900de01cab31b7e269a1ec1b7b320b338
Le Vivilet <levivilet@pm.me>
2025-11-14 18:16:15 +0100

    feature: add better types (#363)

--- packages/title-bar-worker/src/parts/MenuEntriesTitleBar/MenuEntriesTitleBarRemote.ts
+++ packages/title-bar-worker/src/parts/MenuEntriesTitleBar/MenuEntriesTitleBarRemote.ts
@@ -1,51 +1,60 @@
+import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 import * as MenuEntryId from '../MenuEntryId/MenuEntryId.ts'
 import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.ts'
 import * as ViewletTitleBarStrings from '../TitleBarStrings/TitleBarStrings.ts'
 
-export const getMenuEntries = (): any => {
+export const getMenuEntries = (): readonly MenuEntry[] => {
   return [
     {
       id: MenuEntryId.File,
       label: ViewletTitleBarStrings.file(),
       flags: MenuItemFlags.SubMenu,
+      command: '',
     },
     {
       id: MenuEntryId.Edit,
       label: ViewletTitleBarStrings.edit(),
       flags: MenuItemFlags.SubMenu,
+      command: '',
     },
     {
       id: MenuEntryId.Selection,
       label: ViewletTitleBarStrings.selection(),
       flags: MenuItemFlags.SubMenu,
+      command: '',
     },
     {
       id: MenuEntryId.View,
       label: ViewletTitleBarStrings.view(),
       flags: MenuItemFlags.SubMenu,
+      command: '',
     },
     {
       id: MenuEntryId.Go,
       label: ViewletTitleBarStrings.go(),
       flags: MenuItemFlags.SubMenu,
+      command: '',
     },
     {
       id: MenuEntryId.Run,
       label: ViewletTitleBarStrings.run(),
       keyboardShortCut: 'Alt+r',
       flags: MenuItemFlags.SubMenu,
+      command: '',
     },
     {
       id: MenuEntryId.Terminal,
       label: ViewletTitleBarStrings.terminal(),
       keyboardShortCut: 'Alt+t',
       flags: MenuItemFlags.SubMenu,
+      command: '',
     },
     {
       id: MenuEntryId.Help,
       label: ViewletTitleBarStrings.help(),
       keyboardShortCut: 'Alt+h',
       flags: MenuItemFlags.SubMenu,
+      command: '',
     },
   ]
 }
