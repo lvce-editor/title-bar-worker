17903345404ea3331fb0e6429dc81016030e242d
Le Vivilet <levivilet@pm.me>
2025-11-14 18:30:02 +0100

    feature: add context menu id (#369)

--- packages/title-bar-worker/src/parts/GetMenuIds/GetMenuIds.ts
+++ packages/title-bar-worker/src/parts/GetMenuIds/GetMenuIds.ts
@@ -1,7 +1,9 @@
 import { menus } from '../Menus/Menus.ts'
 
+export const MenuIdTitleBarContextMenu = 50
+
 export const getMenuIds = (): readonly number[] => {
-  return menus.map((menu) => menu.id)
+  return [...menus.map((menu) => menu.id), MenuIdTitleBarContextMenu]
 }
 
 export const getMenuEntries = async (id: number, platform: number): Promise<readonly any[]> => {
