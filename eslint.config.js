c5df5c927b4293171fd54099dd24dfe1caaec22c
Le Vivilet <levivilet@pm.me>
2025-11-14 16:29:36 +0100

    feature; lint actions

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import * as config from '@lvce-editor/eslint-config'
+import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
   ...config.default,
+  ...actions.default,
   {
     rules: {
       'jest/no-restricted-jest-methods': 'off',
