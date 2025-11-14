88b8a7e573d31b20e054303fe3418dbe9e31d3db
Le Vivilet <levivilet@pm.me>
2025-11-14 18:55:36 +0100

    feature: update menu descriptions

--- packages/title-bar-worker/src/parts/GetMenuEntriesTitleBarContextMenu/GetMenuEntriesTitleBarContextMenu.ts
+++ packages/title-bar-worker/src/parts/GetMenuEntriesTitleBarContextMenu/GetMenuEntriesTitleBarContextMenu.ts
@@ -1,6 +1,7 @@
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.ts'
+import * as TitleBarStrings from '../TitleBarStrings/TitleBarStrings.ts'
 
 export const getMenuEntriesTitleBarContextMenu = async (state: TitleBarMenuBarState): Promise<readonly MenuEntry[]> => {
   const { titleBarMenuBarEnabled, commandCenterEnabled } = state
@@ -8,13 +9,13 @@ export const getMenuEntriesTitleBarContextMenu = async (state: TitleBarMenuBarSt
   return [
     {
       id: 'MenuBar',
-      label: 'menu bar',
+      label: TitleBarStrings.menuBar(),
       flags: titleBarMenuBarEnabled ? MenuItemFlags.Checked : MenuItemFlags.Unchecked,
       command: titleBarMenuBarEnabled ? 'TitleBar.hideMenuBar' : 'TitleBar.showMenuBar',
     },
     {
       id: 'Command center',
-      label: 'command center',
+      label: TitleBarStrings.commandCenter(),
       flags: commandCenterEnabled ? MenuItemFlags.Checked : MenuItemFlags.Unchecked,
       command: commandCenterEnabled ? 'TitleBar.hideCommandCenter' : 'TitleBar.showCommandCenter',
     },
