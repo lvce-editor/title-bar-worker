50670ecef4a84e4f001ea91861bb82060b6f304e
Le Vivilet <levivilet@pm.me>
2025-11-14 18:05:26 +0100

    feature: add context menu e2e test

---
+++ packages/e2e/src/title-bar-context-menu.ts
@@ -0,0 +1,13 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'title-bar.context-menu'
+
+export const test: Test = async ({ Command }) => {
+  // arrange
+
+  // act
+  await Command.execute('TitleBar.handleContextMenu', 0, 0, 0)
+
+  // assert
+  // TODO verify that context menu is visible
+}
