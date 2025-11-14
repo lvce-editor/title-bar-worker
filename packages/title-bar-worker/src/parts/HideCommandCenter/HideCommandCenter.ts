d911a6725653a8ebc22be5971da307820498a245
Le Vivilet <levivilet@pm.me>
2025-11-14 18:55:25 +0100

    feature: add showCommandCenter (#379)

---
+++ packages/title-bar-worker/src/parts/HideCommandCenter/HideCommandCenter.ts
@@ -0,0 +1,6 @@
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
+
+export const hideCommandCenter = async (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
+  // TODO
+  return state
+}
---
+++ packages/title-bar-worker/src/parts/ShowCommandCenter/ShowCommandCenter.ts
@@ -0,0 +1,6 @@
+import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
+
+export const showCommandCenter = async (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
+  // TODO
+  return state
+}
