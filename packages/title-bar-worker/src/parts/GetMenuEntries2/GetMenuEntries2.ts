611c30e508b160499e5137496acc604c25dd63d8
Le Vivilet <levivilet@pm.me>
2025-11-14 18:34:24 +0100

    feature: update getMenuEntries2 (#374)

--- packages/title-bar-worker/src/parts/GetMenuEntries2/GetMenuEntries2.ts
+++ packages/title-bar-worker/src/parts/GetMenuEntries2/GetMenuEntries2.ts
@@ -1,8 +1,13 @@
 import type { ContextMenuProps } from '../ContextMenuProps/ContextMenuProps.ts'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
+import { getMenuEntriesTitleBarContextMenu } from '../GetMenuEntriesTitleBarContextMenu/GetMenuEntriesTitleBarContextMenu.ts'
+import { MenuIdTitleBarContextMenu } from '../GetMenuIds/GetMenuIds.ts'
 
 export const getMenuEntries2 = async (state: TitleBarMenuBarState, props: ContextMenuProps): Promise<readonly MenuEntry[]> => {
+  if (props.menuId === MenuIdTitleBarContextMenu) {
+    return getMenuEntriesTitleBarContextMenu(state)
+  }
   // TODO
   return []
 }
