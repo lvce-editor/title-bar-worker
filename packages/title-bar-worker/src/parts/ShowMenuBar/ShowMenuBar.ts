9c161a6eaf2c045929af61056d4f1dd5356921e1
Le Vivilet <levivilet@pm.me>
2025-11-14 18:56:03 +0100

    feature: implement showMenuBar (#380)

--- packages/title-bar-worker/src/parts/ShowMenuBar/ShowMenuBar.ts
+++ packages/title-bar-worker/src/parts/ShowMenuBar/ShowMenuBar.ts
@@ -1,6 +1,8 @@
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 
 export const showMenuBar = async (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
-  // TODO
-  return state
+  return {
+    ...state,
+    titleBarMenuBarEnabled: true,
+  }
 }
