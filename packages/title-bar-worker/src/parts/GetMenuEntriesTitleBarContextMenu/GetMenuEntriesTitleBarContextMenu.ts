7638f1d0d07f3be44fc94f38a21f05adc1246ce5
Le Vivilet <levivilet@pm.me>
2025-11-14 18:29:51 +0100

    feature: update context menu entries

--- packages/title-bar-worker/src/parts/GetMenuEntriesTitleBarContextMenu/GetMenuEntriesTitleBarContextMenu.ts
+++ packages/title-bar-worker/src/parts/GetMenuEntriesTitleBarContextMenu/GetMenuEntriesTitleBarContextMenu.ts
@@ -1,10 +1,27 @@
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
+import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.ts'
 
 export const getMenuEntriesTitleBarContextMenu = async (state: TitleBarMenuBarState): Promise<readonly MenuEntry[]> => {
-  // TODO should return 3 options
-  // 1. menu bar (toggle)
-  // 2. command center
-  // 3. layout controls
-  return []
+  // TODO checked state should be depending on whether or not that feature is currently visible or not
+  return [
+    {
+      id: 'MenuBar',
+      label: 'menu bar',
+      flags: MenuItemFlags.Checked,
+      command: '',
+    },
+    {
+      id: 'Command center',
+      label: 'command center',
+      flags: MenuItemFlags.Checked,
+      command: '',
+    },
+    {
+      id: 'layout controls',
+      label: 'layout controls',
+      flags: MenuItemFlags.Checked,
+      command: '',
+    },
+  ]
 }
