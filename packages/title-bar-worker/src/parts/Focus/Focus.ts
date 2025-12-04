7765ea9cbae9509c918dfb2778dc63b019e06304
Le Vivilet <levivilet@pm.me>
2025-12-04 20:38:02 +0100

    feature: update imports for better treeshaking (#401)

--- packages/title-bar-worker/src/parts/Focus/Focus.ts
+++ packages/title-bar-worker/src/parts/Focus/Focus.ts
@@ -1,4 +1,4 @@
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+import { RendererWorker as ParentRpc } from '@lvce-editor/rpc-registry'
 
 export const setFocus = async (focusKey: number): Promise<void> => {
   await ParentRpc.invoke('Focus.setFocus', focusKey)
--- packages/title-bar-worker/src/parts/GetRecentlyOpened/GetRecentlyOpened.ts
+++ packages/title-bar-worker/src/parts/GetRecentlyOpened/GetRecentlyOpened.ts
@@ -1,4 +1,4 @@
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+import { RendererWorker as ParentRpc } from '@lvce-editor/rpc-registry'
 
 export const getRecentlyOpened = (): Promise<any> => {
   return ParentRpc.invoke(/* RecentlyOpened.getRecentlyOpened */ 'RecentlyOpened.getRecentlyOpened')
