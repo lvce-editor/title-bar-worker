aac3c0e7b254004a52aec907f4a53e83cb694b89
Le Vivilet <levivilet@pm.me>
2025-12-04 20:09:31 +0100

    feature: update context menu types (#389)

--- packages/title-bar-worker/src/parts/ContextMenuProps/ContextMenuProps.ts
+++ packages/title-bar-worker/src/parts/ContextMenuProps/ContextMenuProps.ts
@@ -1,5 +1,63 @@
-export interface ContextMenuProps {
-  readonly href: string
+import type { MenuEntryId } from '@lvce-editor/constants'
+
+export interface MenuPropsBase {
   readonly menuId: number
-  readonly nodeName: string
 }
+
+export interface MenuPropsContextMenu extends MenuPropsBase {
+  readonly menuId: typeof MenuEntryId.TitleBarContextMenu | 50
+}
+
+export interface MenuPropsFile extends MenuPropsBase {
+  readonly menuId: typeof MenuEntryId.File
+  readonly platform: number
+}
+
+export interface MenuPropsEdit extends MenuPropsBase {
+  readonly menuId: typeof MenuEntryId.Edit
+}
+
+export interface MenuPropsGo extends MenuPropsBase {
+  readonly menuId: typeof MenuEntryId.Go
+}
+
+export interface MenuPropsHelp extends MenuPropsBase {
+  readonly menuId: typeof MenuEntryId.Help
+}
+
+export interface MenuPropsOpenRecent extends MenuPropsBase {
+  readonly menuId: typeof MenuEntryId.OpenRecent
+}
+
+export interface MenuPropsRun extends MenuPropsBase {
+  readonly menuId: typeof MenuEntryId.Run
+}
+
+export interface MenuPropsSelection extends MenuPropsBase {
+  readonly menuId: typeof MenuEntryId.Selection
+}
+
+export interface MenuPropsTerminal extends MenuPropsBase {
+  readonly menuId: typeof MenuEntryId.Terminal
+}
+
+export interface MenuPropsTitleBar extends MenuPropsBase {
+  readonly menuId: typeof MenuEntryId.TitleBar
+}
+
+export interface MenuPropsView extends MenuPropsBase {
+  readonly menuId: typeof MenuEntryId.View
+}
+
+export type ContextMenuProps =
+  | MenuPropsContextMenu
+  | MenuPropsEdit
+  | MenuPropsFile
+  | MenuPropsSelection
+  | MenuPropsTerminal
+  | MenuPropsOpenRecent
+  | MenuPropsTitleBar
+  | MenuPropsView
+  | MenuPropsRun
+  | MenuPropsGo
+  | MenuPropsHelp
