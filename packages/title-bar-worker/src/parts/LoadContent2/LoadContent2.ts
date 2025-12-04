aa3bc312698844078ac31fc299fa18381c1b7b2e
Le Vivilet <levivilet@pm.me>
2025-12-04 20:59:41 +0100

    feature: simplify loading menu entries (#407)

--- packages/title-bar-worker/src/parts/LoadContent2/LoadContent2.ts
+++ packages/title-bar-worker/src/parts/LoadContent2/LoadContent2.ts
@@ -1,15 +1,14 @@
-import { MenuEntryId } from '@lvce-editor/constants'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as AddWidths from '../AddWidths/AddWidths.ts'
 import { getTitle } from '../GetTitle/GetTitle.ts'
 import { getTitleBarButtons } from '../GetTitleBarButtons/GetTitleBarButtons.ts'
-import * as MenuEntries from '../MenuEntries/MenuEntries.ts'
+import * as MenuEntriesTitleBar from '../MenuEntriesTitleBar/MenuEntriesTitleBar.ts'
 
 export const loadContent2 = async (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
   const { controlsOverlayEnabled, labelFontFamily, labelFontSize, labelFontWeight, labelLetterSpacing, labelPadding, platform, titleBarStyleCustom } =
     state
-  const titleBarEntries = await MenuEntries.getMenuEntries(MenuEntryId.TitleBar)
+  const titleBarEntries = await MenuEntriesTitleBar.getMenuEntries(platform)
   const withWidths = AddWidths.addWidths(titleBarEntries, labelPadding, labelFontWeight, labelFontSize, labelFontFamily, labelLetterSpacing)
   const buttons = getTitleBarButtons(platform, controlsOverlayEnabled, titleBarStyleCustom)
   const workspaceUri = await RendererWorker.invoke('Workspace.getUri')
