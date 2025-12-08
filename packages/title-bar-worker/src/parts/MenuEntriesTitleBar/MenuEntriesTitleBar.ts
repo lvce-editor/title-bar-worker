3b78e9fab282b392cb12240bf3fb6b7e6a048027
Le Vivilet <levivilet@pm.me>
2025-12-08 13:02:52 +0100

    feature: add handler types

--- packages/title-bar-worker/src/parts/MenuEntriesTitleBar/MenuEntriesTitleBar.ts
+++ packages/title-bar-worker/src/parts/MenuEntriesTitleBar/MenuEntriesTitleBar.ts
@@ -3,7 +3,11 @@ import * as PlatformType from '../PlatformType/PlatformType.ts'
 import * as MenuEntriesTitleBarRemote from './MenuEntriesTitleBarRemote.ts'
 import * as MenuEntriesTitleBarWeb from './MenuEntriesTitleBarWeb.ts'
 
-const getFn = (platform: number): any => {
+interface Handler {
+  (): readonly any[]
+}
+
+const getFn = (platform: number): Handler => {
   switch (platform) {
     case PlatformType.Web:
       return MenuEntriesTitleBarWeb.getMenuEntries
