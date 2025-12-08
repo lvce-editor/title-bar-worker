38bc73e706fbcaa98e68531268dedad52d7c286f
Le Vivilet <levivilet@pm.me>
2025-12-08 11:29:14 +0100

    feature: move editor strings

--- packages/title-bar-worker/src/parts/EditorStrings/EditorStrings.ts
+++ packages/title-bar-worker/src/parts/EditorStrings/EditorStrings.ts
@@ -1,64 +1,5 @@
 import * as I18nString from '../I18NString/I18NString.ts'
-
-/**
- * @enum {string}
- */
-const UiStrings = {
-  AddCursorAbove: 'Add Cursor Above',
-  AddCursorBelow: 'Add Cursor Below',
-  AddCursorsToLineEnds: 'Add Cursors to Line ends',
-  AddNextOccurrence: 'Add Next Occurrence',
-  AddPreviousOccurrence: 'Add Previous Occurrence',
-  Copy: 'Copy',
-  CopyLineDown: 'Copy Line Down',
-  CopyLineUp: 'Copy Line Up',
-  Cut: 'Cut',
-  DuplicateSelection: 'Duplicate Selection',
-  EditorCloseColorPicker: 'Editor: Close Color Picker',
-  EditorCopyLineDown: 'Editor: Copy Line Down',
-  EditorCopyLineUp: 'Editor: Copy Line Up',
-  EditorFormatDocumentForced: 'Editor: Format Document (forced)',
-  EditorGoToDefinition: 'Editor: Go To Definition',
-  EditorGoToTypeDefinition: 'Editor: Go To Type Definition',
-  EditorIndent: 'Editor: Indent',
-  EditorOpenColorPicker: 'Editor: Open Color Picker',
-  EditorSelectAllOccurrences: 'Editor: Select All Occurrences',
-  EditorSelectDown: 'Editor: Select Down',
-  EditorSelectInsideString: 'Editor: Select Inside String',
-  EditorSelectNextOccurrence: 'Editor: Select Next Occurrence',
-  EditorSelectUp: 'Editor: Select Up',
-  EditorShowHover: 'Show Hover',
-  EditorSortLinesAscending: 'Editor: Sort Lines Ascending',
-  EditorToggleBlockComment: 'Editor: Toggle Block Comment',
-  EditorToggleComment: 'Editor: Toggle Comment',
-  EditorUnindent: 'Editor: Unindent',
-  ExpandSelection: 'Expand Selection',
-  FindAllImplementations: 'Find All Implementations',
-  FindAllReferences: 'Find All References',
-  FormatDocument: 'Format Document',
-  GoToDefinition: 'Go to Definition',
-  GoToTypeDefinition: 'Go to Type Definition',
-  MoveLineDown: 'Move Line Down',
-  MoveLineUp: 'Move Line Up',
-  NoDefinitionFound: 'No definition found',
-  NoDefinitionFoundFor: "No definition found for '{PH1}'",
-  NoResults: 'No Results',
-  NoTypeDefinitionFound: 'No type definition found',
-  NoTypeDefinitionFoundFor: "No type definition found for '{PH1}'",
-  OrganizeImports: 'Organize Imports',
-  Paste: 'Paste',
-  Redo: 'Redo',
-  Replace: 'replace',
-  SelectAll: 'Select All',
-  SelectAllOccurrences: 'Select All Occurrences',
-  Separator: 'Separator',
-  ShrinkSelection: 'Shrink Selection',
-  SortImports: 'Sort Imports',
-  SourceActions: 'Source Actions',
-  ToggleBlockComment: 'Toggle Block Comment',
-  ToggleLineComment: 'Toggle Line Comment',
-  Undo: 'Undo',
-}
+import * as UiStrings from '../UiStrings/UiStrings.ts'
 
 export const cut = (): string => {
   return I18nString.i18nString(UiStrings.Cut)
@@ -93,7 +34,7 @@ export const selectAll = (): string => {
 }
 
 export const expandSelection = (): string => {
-  return I18nString.i18nString(UiStrings.SelectAll)
+  return I18nString.i18nString(UiStrings.ExpandSelection)
 }
 
 export const shrinkSelection = (): string => {
@@ -117,7 +58,7 @@ export const moveLineDown = (): string => {
 }
 
 export const duplicateSelection = (): string => {
-  return I18nString.i18nString(UiStrings.MoveLineDown)
+  return I18nString.i18nString(UiStrings.DuplicateSelection)
 }
 
 export const addCursorAbove = (): string => {
@@ -125,11 +66,11 @@ export const addCursorAbove = (): string => {
 }
 
 export const addCursorBelow = (): string => {
-  return I18nString.i18nString(UiStrings.AddCursorAbove)
+  return I18nString.i18nString(UiStrings.AddCursorBelow)
 }
 
 export const addCursorsToLineEnds = (): string => {
-  return I18nString.i18nString(UiStrings.AddCursorAbove)
+  return I18nString.i18nString(UiStrings.AddCursorsToLineEnds)
 }
 
 export const addNextOccurrence = (): string => {
