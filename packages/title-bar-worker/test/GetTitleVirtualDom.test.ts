72e1087be04f0a41424aa04656a80950999b3dbd
Le Vivilet <levivilet@pm.me>
2025-12-08 11:51:52 +0100

    test

--- packages/title-bar-worker/test/GetTitleVirtualDom.test.ts
+++ packages/title-bar-worker/test/GetTitleVirtualDom.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@jest/globals'
 import * as GetTitleVirtualDom from '../src/parts/GetTitleVirtualDom/GetTitleVirtualDom.ts'
 
 test('getTitleVirtualDom', () => {
-  expect(GetTitleVirtualDom.getTitleVirtualDom('Test Title')).toEqual(expect.any(Array))
+  expect(GetTitleVirtualDom.getTitleVirtualDom(true, 'Test Title')).toEqual(expect.any(Array))
 })
