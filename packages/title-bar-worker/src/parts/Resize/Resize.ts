52cbbc2dbf19fcaaa2ea554b00d1ac51c9462238
Le Vivilet <levivilet@pm.me>
2025-12-08 13:02:27 +0100

    feature: add dimensions types (#434)

--- packages/title-bar-worker/src/parts/Resize/Resize.ts
+++ packages/title-bar-worker/src/parts/Resize/Resize.ts
@@ -5,7 +5,14 @@ const getTitleBarMenuBarWidth = (width: number, menuBarX: number, titleBarButton
   return remainingWidth
 }
 
-export const resize = async (state: TitleBarMenuBarState, dimensions: any): Promise<TitleBarMenuBarState> => {
+interface Dimensions {
+  readonly height: number
+  readonly width: number
+  readonly x: number
+  readonly y: number
+}
+
+export const resize = async (state: TitleBarMenuBarState, dimensions: Dimensions): Promise<TitleBarMenuBarState> => {
   const { titleBarButtonsWidth } = state
   const menuBarX = dimensions.x
   const menuBarY = dimensions.y
