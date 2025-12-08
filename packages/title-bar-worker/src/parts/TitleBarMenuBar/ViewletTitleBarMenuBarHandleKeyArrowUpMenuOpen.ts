05d7d1219418ae3d45f3f34896d83e5bd3ae3655
Le Vivilet <levivilet@pm.me>
2025-12-08 12:55:49 +0100

    feature: handle undefined menu entry

--- packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleKeyArrowUpMenuOpen.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleKeyArrowUpMenuOpen.ts
@@ -4,6 +4,9 @@ import * as Menu from '../Menu/Menu.ts'
 export const handleKeyArrowUpMenuOpen = (state: TitleBarMenuBarState): TitleBarMenuBarState => {
   const { menus } = state
   const menu = menus.at(-1)
+  if (!menu) {
+    return state
+  }
   const previousIndex = Menu.getIndexToFocusPrevious(menu)
   const newMenus = [
     ...menus.slice(0, -1),
