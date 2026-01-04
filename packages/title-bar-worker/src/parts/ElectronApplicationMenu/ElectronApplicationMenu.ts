ed819fe43f5f62d498b52b640e460e47902c618c
Le Vivilet <levivilet@pm.me>
2026-01-04 19:46:43 +0100

    feature: simplify application menu creation (#452)

--- packages/title-bar-worker/src/parts/ElectronApplicationMenu/ElectronApplicationMenu.ts
+++ packages/title-bar-worker/src/parts/ElectronApplicationMenu/ElectronApplicationMenu.ts
@@ -1,29 +1,10 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import { getEntryMap } from '../GetMenuEntryMap/GetMenuEntryMap.ts'
+import { getMenuIds } from '../GetMenuIds/GetMenuIds.ts'
 import { getWindowId } from '../GetWindowId/GetWindowId.ts'
-import * as MenuEntriesEdit from '../MenuEntriesEdit/MenuEntriesEdit.ts'
-import * as MenuEntriesFile from '../MenuEntriesFile/MenuEntriesFile.ts'
-import * as MenuEntriesGo from '../MenuEntriesGo/MenuEntriesGo.ts'
-import * as MenuEntriesHelp from '../MenuEntriesHelp/MenuEntriesHelp.ts'
-import * as MenuEntriesRun from '../MenuEntriesRun/MenuEntriesRun.ts'
-import * as MenuEntriesSelection from '../MenuEntriesSelection/MenuEntriesSelection.ts'
-import * as MenuEntriesTerminal from '../MenuEntriesTerminal/MenuEntriesTerminal.ts'
-import * as MenuEntriesView from '../MenuEntriesView/MenuEntriesView.ts'
 import * as MenuEntryId from '../MenuEntryId/MenuEntryId.ts'
 import * as ToElectronMenu from '../ToElectronMenu/ToElectronMenu.ts'
-
-const menuEntries = [
-  MenuEntriesFile,
-  MenuEntriesEdit,
-  MenuEntriesSelection,
-  MenuEntriesView,
-  MenuEntriesGo,
-  MenuEntriesRun,
-  MenuEntriesTerminal,
-  MenuEntriesHelp,
-  // MenuEntriesTitleBar,
-]
 
 const setItems = async (items: readonly any[]): Promise<void> => {
   const windowId = await getWindowId()
@@ -31,7 +12,8 @@ const setItems = async (items: readonly any[]): Promise<void> => {
 }
 
 export const hydrate = async (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
-  const map = await getEntryMap(menuEntries)
+  const ids = getMenuIds()
+  const map = await getEntryMap(state, ids)
   const { commandMap, electronMenu } = ToElectronMenu.toElectronMenu(map, MenuEntryId.TitleBar)
   await setItems(electronMenu)
   // TODO get all menu items
