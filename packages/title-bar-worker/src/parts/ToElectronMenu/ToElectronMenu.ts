7650fe9a9d23364b755fef62520bbb0e9488e137
Le Vivilet <levivilet@pm.me>
2026-01-04 21:52:53 +0100

    feature: add menu types

--- packages/title-bar-worker/src/parts/ToElectronMenu/ToElectronMenu.ts
+++ packages/title-bar-worker/src/parts/ToElectronMenu/ToElectronMenu.ts
@@ -1,6 +1,11 @@
 import * as ToElectronMenuInternal from '../ToElectronMenuInternal/ToElectronMenuInternal.ts'
 
-export const toElectronMenu = (map: any, rootId: any): any => {
+export interface ElectronMenuResult {
+  readonly commandMap: any
+  readonly electronMenu: any
+}
+
+export const toElectronMenu = (map: any, rootId: number): any => {
   const electronMenu: readonly any[] = []
   const commandMap = Object.create(null)
   ToElectronMenuInternal.toElectronMenuInternal(commandMap, map, rootId, electronMenu)
