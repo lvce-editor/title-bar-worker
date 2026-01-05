a8ed8c8290bba3c082b6c1886c0f9bd62cdca19b
Le Vivilet <levivilet@pm.me>
2026-01-05 19:14:18 +0100

    feature: modernize rpc (#470)

--- packages/title-bar-worker/src/parts/LaunchTextMeasurementWorker/LaunchTextMeasurementWorker.ts
+++ packages/title-bar-worker/src/parts/LaunchTextMeasurementWorker/LaunchTextMeasurementWorker.ts
@@ -1,13 +1,22 @@
-import type { Rpc } from '@lvce-editor/rpc'
 import { TransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const launchTextMeasurementWorker = async (): Promise<Rpc> => {
+interface Rpc2 {
+  readonly invoke: (method: string, ...params: readonly any[]) => Promise<any>
+  readonly [Symbol.asyncDispose]: () => Promise<void>
+}
+
+export const launchTextMeasurementWorker = async (): Promise<Rpc2> => {
   const rpc = await TransferMessagePortRpcParent.create({
     commandMap: {},
     async send(port) {
       await RendererWorker.sendMessagePortToTextMeasurementWorker(port)
     },
   })
-  return rpc
+  return {
+    invoke: rpc.invoke.bind(rpc),
+    async [Symbol.asyncDispose](): Promise<void> {
+      await rpc.dispose()
+    },
+  }
 }
