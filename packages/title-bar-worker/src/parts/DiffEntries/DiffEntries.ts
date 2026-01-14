de619130158a6bffb29ff087cb546c67ceae8793
Le Vivilet <levivilet@pm.me>
2026-01-15 00:50:41 +0100

    fix

--- packages/title-bar-worker/src/parts/DiffEntries/DiffEntries.ts
+++ packages/title-bar-worker/src/parts/DiffEntries/DiffEntries.ts
@@ -1,5 +1,4 @@
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
-import * as DiffType from '../DiffType/DiffType.ts'
 
 export const isEqual = (oldState: TitleBarMenuBarState, newState: TitleBarMenuBarState): boolean => {
   return (
