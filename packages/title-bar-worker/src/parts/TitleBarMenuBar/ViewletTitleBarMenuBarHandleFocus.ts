f05d15e29a702c84f3686d1142a8eebdbda293a6
Le Vivilet <levivilet@pm.me>
2026-01-15 00:55:57 +0100

    feature: use functional rendering for focus context (#488)

--- packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleFocus.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleFocus.ts
@@ -1,9 +1,6 @@
-import { WhenExpression } from '@lvce-editor/constants'
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
-import * as Focus from '../Focus/Focus.ts'
 
 export const handleFocus = async (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
-  await Focus.setFocus(WhenExpression.FocusTitleBarMenuBar)
   return {
     ...state,
     focused: true,
--- packages/title-bar-worker/test/ViewletTitleBarMenuBarHandleFocus.test.ts
+++ packages/title-bar-worker/test/ViewletTitleBarMenuBarHandleFocus.test.ts
@@ -1,16 +1,10 @@
 import { expect, test } from '@jest/globals'
-import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { TitleBarMenuBarState } from '../src/parts/TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as ViewletTitleBarMenuBarHandleFocus from '../src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarHandleFocus.ts'
 
 test('handleFocus sets focus and returns same state', async () => {
-  const mockRpc = RendererWorker.registerMockRpc({
-    'Focus.setFocus'() {},
-  })
-
   const state: TitleBarMenuBarState = createDefaultState()
   const result = await ViewletTitleBarMenuBarHandleFocus.handleFocus(state)
   expect(result).toEqual({ ...state, focused: true })
-  expect(mockRpc.invocations).toEqual([['Focus.setFocus', 26]])
 })
