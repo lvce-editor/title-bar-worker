8cbfeebfd8ce8e5c54ad637626aa1590cfe4cd12
Le Vivilet <levivilet@pm.me>
2026-01-15 00:20:13 +0100

    feature: update module registration (#484)

--- packages/title-bar-worker/src/parts/GetRenderer3/GetRenderer3.ts
+++ packages/title-bar-worker/src/parts/GetRenderer3/GetRenderer3.ts
@@ -1,5 +1,6 @@
 import type { Renderer } from '../Renderer/Renderer.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
+import { renderFocusContext } from '../RenderFocusContext/RenderFocusContext.ts'
 import * as RenderFocusedIndex from '../RenderFocusedIndex/RenderFocusedIndex.ts'
 import * as RenderMenus from '../RenderMenus/RenderMenus.ts'
 import * as RenderTitleBar from '../RenderTitleBar/RenderTitleBar.ts'
@@ -8,6 +9,8 @@ export const getRenderer3 = (diffType: number): Renderer => {
   switch (diffType) {
     case DiffType.RenderEntries:
       return RenderTitleBar.renderTitleBar
+    case DiffType.RenderFocusContext:
+      return renderFocusContext
     case DiffType.RenderFocusedIndex:
       return RenderFocusedIndex.renderFocusedIndex
     case DiffType.RenderMenus:
