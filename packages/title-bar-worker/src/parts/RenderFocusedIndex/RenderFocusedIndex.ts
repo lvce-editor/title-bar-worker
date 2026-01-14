2a756167ba7f740335a2a994a2d4c0213ceee633
Le Vivilet <levivilet@pm.me>
2026-01-14 23:57:08 +0100

    feature: use enum (#480)

--- packages/title-bar-worker/src/parts/RenderFocusedIndex/RenderFocusedIndex.ts
+++ packages/title-bar-worker/src/parts/RenderFocusedIndex/RenderFocusedIndex.ts
@@ -1,8 +1,9 @@
+import { ViewletCommand } from '@lvce-editor/constants'
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 
 export const renderFocusedIndex = (oldState: TitleBarMenuBarState, newState: TitleBarMenuBarState): readonly any[] => {
   if (newState.focusedIndex === -1) {
     return []
   }
-  return ['Viewlet.focusSelector', '.ViewletTitleBarMenuBar']
+  return [ViewletCommand.FocusSelector, '.ViewletTitleBarMenuBar']
 }
--- packages/title-bar-worker/src/parts/RenderTitleBar/RenderTitleBar.ts
+++ packages/title-bar-worker/src/parts/RenderTitleBar/RenderTitleBar.ts
@@ -1,7 +1,8 @@
+import { ViewletCommand } from '@lvce-editor/constants'
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as GetTitleBarVirtualDom from '../GetTitleBarVirtualDom/GetTitleBarVirtualDom.ts'
 
 export const renderTitleBar = (oldState: TitleBarMenuBarState, newState: TitleBarMenuBarState): readonly any[] => {
   const dom = GetTitleBarVirtualDom.getTitleBarVirtualDom(newState)
-  return ['Viewlet.setDom2', newState.uid, dom]
+  return [ViewletCommand.SetDom2, newState.uid, dom]
 }
