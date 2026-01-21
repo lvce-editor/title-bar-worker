12379c56c4248d70bd89f8d1866bbc079714cc9b
Le Vivilet <levivilet@pm.me>
2026-01-21 22:06:26 +0100

    feature: update title on workspace change (#499)

--- packages/title-bar-worker/src/parts/HandleWorkspaceChange/HandleWorkspaceChange.ts
+++ packages/title-bar-worker/src/parts/HandleWorkspaceChange/HandleWorkspaceChange.ts
@@ -1,9 +1,12 @@
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
+import { getTitle } from '../GetTitle/GetTitle.ts'
 
 // TODO in the future, it could also be a multi-root workspace
 export const handleWorkspaceChange = async (state: TitleBarMenuBarState, uri: string): Promise<TitleBarMenuBarState> => {
+  const title = getTitle(uri)
   return {
     ...state,
+    title,
     workspaceUri: uri,
   }
 }
