ea99e5f831d5a776d8f2021798ca92f7a6766e19
Le Vivilet <levivilet@pm.me>
2026-01-21 22:03:43 +0100

    feature: move getworkspaceuri into separate file (#498)

--- packages/title-bar-worker/src/parts/LoadContent2/LoadContent2.ts
+++ packages/title-bar-worker/src/parts/LoadContent2/LoadContent2.ts
@@ -1,10 +1,10 @@
 import { PlatformType } from '@lvce-editor/constants'
-import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as AddWidths from '../AddWidths/AddWidths.ts'
 import { hydrate } from '../ElectronApplicationMenu/ElectronApplicationMenu.ts'
 import { getTitle } from '../GetTitle/GetTitle.ts'
 import { getTitleBarButtons } from '../GetTitleBarButtons/GetTitleBarButtons.ts'
+import { getWorkspaceUri } from '../GetWorkspaceUri/GetWorkspaceUri.ts'
 import * as MenuEntriesTitleBar from '../MenuEntriesTitleBar/MenuEntriesTitleBar.ts'
 
 export const loadContent2 = async (state: TitleBarMenuBarState): Promise<TitleBarMenuBarState> => {
@@ -13,7 +13,7 @@ export const loadContent2 = async (state: TitleBarMenuBarState): Promise<TitleBa
   const titleBarEntries = await MenuEntriesTitleBar.getMenuEntries(platform)
   const withWidths = await AddWidths.addWidths(titleBarEntries, labelPadding, labelFontWeight, labelFontSize, labelFontFamily, labelLetterSpacing)
   const buttons = getTitleBarButtons(platform, controlsOverlayEnabled, titleBarStyleCustom)
-  const workspaceUri = await RendererWorker.invoke('Workspace.getUri')
+  const workspaceUri = await getWorkspaceUri()
   const title = getTitle(workspaceUri)
   const iconWidth = 30
 
