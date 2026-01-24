545f7b280ef7e4987e0f9f84087b84a2b24a001b
Le Vivilet <levivilet@pm.me>
2026-01-25 00:13:51 +0100

    feature: add template e2e tests (#508)
    * feature: add template e2e tests
    
    * feature: add template e2e tests

---
+++ packages/e2e/src/title-bar-title-template-long.ts
@@ -0,0 +1,22 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'title-bar-title-template-long'
+
+export const skip = true
+
+export const test: Test = async ({ Command, expect, FileSystem, Locator, Workspace }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await Workspace.setPath(`${tmpDir}/my-project`)
+
+  // act
+  await Command.execute(
+    'TitleBar.setTitleTemplate',
+    'This is a very long title template that should still be displayed correctly in the title bar without any issues',
+  )
+
+  // assert
+  const title = Locator('.TitleBarTitle')
+  await expect(title).toBeVisible()
+  await expect(title).toHaveText('This is a very long title template that should still be displayed correctly in the title bar without any issues')
+}
