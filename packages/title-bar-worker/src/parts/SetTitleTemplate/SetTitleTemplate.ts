01ed2925c430229e54487e2d019d3c374d27a5be
Le Vivilet <levivilet@pm.me>
2026-01-29 02:09:23 +0100

    feature: update setTitleTemplate to include title generation

--- packages/title-bar-worker/src/parts/SetTitleTemplate/SetTitleTemplate.ts
+++ packages/title-bar-worker/src/parts/SetTitleTemplate/SetTitleTemplate.ts
@@ -1,8 +1,13 @@
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
+import { getTitle } from '../GetTitle/GetTitle.ts'
+
+const APP_NAME = 'Lvce Editor'
 
 export const setTitleTemplate = (state: TitleBarMenuBarState, titleTemplate: string): TitleBarMenuBarState => {
+  const title = getTitle(state.workspaceUri, titleTemplate, APP_NAME)
   return {
     ...state,
+    title,
     titleTemplate,
   }
 }
