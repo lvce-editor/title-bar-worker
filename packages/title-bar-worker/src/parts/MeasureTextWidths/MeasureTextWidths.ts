d914a3cc0a54d05f979a4b6b33ca067c0437f82e
Le Vivilet <levivilet@pm.me>
2026-03-29 09:48:34 +0200

    refactor: simplify measureTextWidths function by removing fallback implementation

--- packages/title-bar-worker/src/parts/MeasureTextWidths/MeasureTextWidths.ts
+++ packages/title-bar-worker/src/parts/MeasureTextWidths/MeasureTextWidths.ts
@@ -1,29 +1,4 @@
-import { px } from '@lvce-editor/virtual-dom-worker'
-import { createTextMeasureContext } from '../CreateTextMeasureContext/CreateTextMeasureContext.ts'
-import * as GetFontString from '../GetFontString/GetFontString.ts'
 import { measureTextWidths2 } from '../MeasureTextWidths2/MeasureTextWidths2.ts'
-
-const measureTextWidthsOld = async (
-  texts: readonly string[],
-  fontWeight: number,
-  fontSize: number,
-  fontFamily: string,
-  letterSpacing: number,
-): Promise<readonly number[]> => {
-  if (typeof letterSpacing !== 'number') {
-    throw new TypeError('letterSpacing must be of type number')
-  }
-  const letterSpacingString = px(letterSpacing)
-  const fontString = GetFontString.getFontString(fontWeight, fontSize, fontFamily)
-  const ctx = createTextMeasureContext(letterSpacingString, fontString)
-  const widths: number[] = []
-  for (const text of texts) {
-    const metrics = ctx.measureText(text)
-    const { width } = metrics
-    widths.push(width)
-  }
-  return widths
-}
 
 export const measureTextWidths = async (
   texts: readonly string[],
@@ -31,10 +6,4 @@ export const measureTextWidths = async (
   fontSize: number,
   fontFamily: string,
   letterSpacing: number,
-): Promise<readonly number[]> => {
-  try {
-    return measureTextWidths2(texts, fontWeight, fontSize, fontFamily, letterSpacing)
-  } catch {
-    return measureTextWidthsOld(texts, fontWeight, fontSize, fontFamily, letterSpacing)
-  }
-}
+): Promise<readonly number[]> => measureTextWidths2(texts, fontWeight, fontSize, fontFamily, letterSpacing)
