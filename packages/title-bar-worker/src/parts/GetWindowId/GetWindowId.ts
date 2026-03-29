bec7681450c100e3b958934715f3ab2eb1bcf7a8
Le Vivilet <levivilet@pm.me>
2026-03-29 09:06:31 +0200

    fix: correct method invocation for getting window ID (#554)

--- packages/title-bar-worker/src/parts/GetWindowId/GetWindowId.ts
+++ packages/title-bar-worker/src/parts/GetWindowId/GetWindowId.ts
@@ -3,6 +3,6 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const getWindowId = async (): Promise<number> => {
-  const windowId = await RendererWorker.invoke('GetWindowId.getWindowId')
+  const windowId = await RendererWorker.getWindowId()
   return windowId
 }
