0f76719ca11571fe8081142b316af3568ef0a8fa
Le Vivilet <levivilet@pm.me>
2026-04-16 22:12:04 +0200

    fix

--- packages/title-bar-worker/src/parts/GetNavigableTitleBarEntries/GetNavigableTitleBarEntries.ts
+++ packages/title-bar-worker/src/parts/GetNavigableTitleBarEntries/GetNavigableTitleBarEntries.ts
@@ -1,19 +1,8 @@
 import type { ComputedTitleBarEntry, TitleBarEntry } from '../TitleBarEntry/TitleBarEntry.ts'
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
-import { getVisibleTitleBarEntries, OverflowMenuId } from '../GetVisibleTitleBarEntries/GetVisibleTitleBarEntries.ts'
-
-interface EntryWithId {
-  readonly id?: string | number
-}
-
-const hasOverflowEntry = (entries: readonly EntryWithId[]): boolean => {
-  return entries.some((entry) => entry?.id === OverflowMenuId)
-}
+import { getVisibleTitleBarEntries } from '../GetVisibleTitleBarEntries/GetVisibleTitleBarEntries.ts'
 
 export const getNavigableTitleBarEntries = (state: TitleBarMenuBarState): readonly ComputedTitleBarEntry<TitleBarEntry>[] => {
   const { focusedIndex, isMenuOpen, titleBarEntries, width } = state
-  if (hasOverflowEntry(titleBarEntries)) {
-    return titleBarEntries as readonly ComputedTitleBarEntry<TitleBarEntry>[]
-  }
   return getVisibleTitleBarEntries(titleBarEntries, width, focusedIndex, isMenuOpen)
 }
