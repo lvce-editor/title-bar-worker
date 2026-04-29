c7d5891fda295d122d5a3bf25df00b4831b9b866
Le Vivilet <levivilet@pm.me>
2026-04-29 09:59:29 +0200

    feature: enhance title bar test to verify title updates on workspace change (#573)

--- packages/e2e/src/title-bar-update-title-on-workspace-change.ts
+++ packages/e2e/src/title-bar-update-title-on-workspace-change.ts
@@ -5,12 +5,19 @@ export const name = 'title-bar-update-title-on-workspace-change'
 export const test: Test = async ({ expect, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir1 = await FileSystem.getTmpDir()
+  const tmpDir2 = await FileSystem.getTmpDir()
+  const title = Locator('.TitleBarTitle')
 
   // act
   await Workspace.setPath(`${tmpDir1}/folder-1`)
 
   // assert
-  const title = Locator('.TitleBarTitle')
   await expect(title).toBeVisible()
   await expect(title).toHaveText('folder-1')
+
+  // act
+  await Workspace.setPath(`${tmpDir2}/folder-2`)
+
+  // assert
+  await expect(title).toHaveText('folder-2')
 }
