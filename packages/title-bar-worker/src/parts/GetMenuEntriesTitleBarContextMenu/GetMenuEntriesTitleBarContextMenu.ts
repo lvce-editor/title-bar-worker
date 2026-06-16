ca09971fc1e6aa6c7f338d8bd23d9d549e993273
Le Vivilet <levivilet@pm.me>
2026-06-16 18:12:46 +0200

    fix

--- packages/title-bar-worker/src/parts/GetMenuEntriesTitleBarContextMenu/GetMenuEntriesTitleBarContextMenu.ts
+++ packages/title-bar-worker/src/parts/GetMenuEntriesTitleBarContextMenu/GetMenuEntriesTitleBarContextMenu.ts
@@ -5,17 +5,29 @@ import * as TitleBarStrings from '../TitleBarStrings/TitleBarStrings.ts'
 
 export const getMenuEntriesTitleBarContextMenu = async (state: TitleBarMenuBarState): Promise<readonly MenuEntry[]> => {
   const { commandCenterEnabled, titleBarMenuBarEnabled } = state
+  let menuBarCommand = 'TitleBar.showMenuBar'
+  let menuBarFlags = MenuItemFlags.Unchecked
+  if (titleBarMenuBarEnabled) {
+    menuBarCommand = 'TitleBar.hideMenuBar'
+    menuBarFlags = MenuItemFlags.Checked
+  }
+  let commandCenterCommand = 'TitleBar.showCommandCenter'
+  let commandCenterFlags = MenuItemFlags.Unchecked
+  if (commandCenterEnabled) {
+    commandCenterCommand = 'TitleBar.hideCommandCenter'
+    commandCenterFlags = MenuItemFlags.Checked
+  }
   // TODO checked state should be depending on whether or not that feature is currently visible or not
   return [
     {
-      command: `TitleBar.${titleBarMenuBarEnabled ? 'hide' : 'show'}MenuBar`,
-      flags: titleBarMenuBarEnabled ? MenuItemFlags.Checked : MenuItemFlags.Unchecked,
+      command: menuBarCommand,
+      flags: menuBarFlags,
       id: 'MenuBar',
       label: TitleBarStrings.menuBar(),
     },
     {
-      command: `TitleBar.${commandCenterEnabled ? 'hide' : 'show'}CommandCenter`,
-      flags: commandCenterEnabled ? MenuItemFlags.Checked : MenuItemFlags.Unchecked,
+      command: commandCenterCommand,
+      flags: commandCenterFlags,
       id: 'Command center',
       label: TitleBarStrings.commandCenter(),
     },
