f122ef2a46a97127229d95426c4353d3b100dac9
Le Vivilet <levivilet@pm.me>
2026-06-30 21:44:52 +0200

    feature: deduplicate recently opened

--- packages/title-bar-worker/src/parts/GetRecentlyOpened/GetRecentlyOpened.ts
+++ packages/title-bar-worker/src/parts/GetRecentlyOpened/GetRecentlyOpened.ts
@@ -1,5 +1,34 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const getRecentlyOpened = (): Promise<any> => {
-  return RendererWorker.invoke(/* RecentlyOpened.getRecentlyOpened */ 'RecentlyOpened.getRecentlyOpened')
+const normalizeUri = (value: string): string => {
+  try {
+    const url = new URL(value)
+    if (/^[a-zA-Z]:$/.test(url.protocol)) {
+      return ''
+    }
+    return url.href
+  } catch {
+    return ''
+  }
+}
+
+export const getRecentlyOpened = async (): Promise<readonly string[]> => {
+  const recentlyOpened: unknown = await RendererWorker.invoke(/* RecentlyOpened.getRecentlyOpened */ 'RecentlyOpened.getRecentlyOpened')
+  if (!Array.isArray(recentlyOpened)) {
+    return []
+  }
+  const seen = new Set<string>()
+  const uniqueUris: string[] = []
+  for (const item of recentlyOpened) {
+    if (typeof item !== 'string') {
+      continue
+    }
+    const uri = normalizeUri(item)
+    if (!uri || seen.has(uri)) {
+      continue
+    }
+    seen.add(uri)
+    uniqueUris.push(uri)
+  }
+  return uniqueUris
 }
