892504f0203ddb8b34f73d2560d40ccf2ea6d190
Le Vivilet <levivilet@pm.me>
2026-06-30 21:53:20 +0200

    feature: update dependencies (#584) (#587)
    * feature: deduplicate recently opened
    
    * fix get recently opened lint

--- packages/title-bar-worker/src/parts/GetRecentlyOpened/GetRecentlyOpened.ts
+++ packages/title-bar-worker/src/parts/GetRecentlyOpened/GetRecentlyOpened.ts
@@ -1,5 +1,36 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const getRecentlyOpened = (): Promise<any> => {
-  return RendererWorker.invoke(/* RecentlyOpened.getRecentlyOpened */ 'RecentlyOpened.getRecentlyOpened')
+const DriveLetterProtocolRegex = /^[a-zA-Z]:$/
+
+const normalizeUri = (value: string): string => {
+  try {
+    const url = new URL(value)
+    if (DriveLetterProtocolRegex.test(url.protocol)) {
+      return ''
+    }
+    return url.href
+  } catch {
+    return ''
+  }
+}
+
+export const getRecentlyOpened = async (): Promise<readonly string[]> => {
+  const recentlyOpened: unknown = await RendererWorker.invoke(/* RecentlyOpened.getRecentlyOpened */ 'RecentlyOpened.getRecentlyOpened')
+  if (!Array.isArray(recentlyOpened)) {
+    return []
+  }
+  const seen = new Set<string>()
+  const uniqueUris: string[] = []
+  for (const item of recentlyOpened) {
+    if (typeof item !== 'string') {
+      continue
+    }
+    const uri = normalizeUri(item)
+    if (!uri || seen.has(uri)) {
+      continue
+    }
+    seen.add(uri)
+    uniqueUris.push(uri)
+  }
+  return uniqueUris
 }
