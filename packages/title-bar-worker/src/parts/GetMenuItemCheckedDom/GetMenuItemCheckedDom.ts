ccdc9fa6f174aa418e3725d7b53fb90dac5fefcb
Le Vivilet <levivilet@pm.me>
2026-07-11 00:08:35 +0200

    fix: enable autosave menu hover (#604)
    * fix: enable autosave menu hover
    
    * fix: allow checked autosave hover test
    
    * fix: defer autosave hover e2e

--- packages/title-bar-worker/src/parts/GetMenuItemCheckedDom/GetMenuItemCheckedDom.ts
+++ packages/title-bar-worker/src/parts/GetMenuItemCheckedDom/GetMenuItemCheckedDom.ts
@@ -1,18 +1,9 @@
-import { VirtualDomElements, AriaRoles } from '@lvce-editor/virtual-dom-worker'
+import { AriaRoles, mergeClassNames, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import type { VisibleMenuItem } from '../VisibleMenuItem/VisibleMenuItem.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
-
-const checkboxChecked: VirtualDomNode = {
-  ariaChecked: true,
-  childCount: 2,
-  className: MergeClassNames.mergeClassNames(ClassNames.MenuItem, ClassNames.MenuItemCheckMark),
-  role: AriaRoles.MenuItemCheckBox,
-  tabIndex: -1,
-  type: VirtualDomElements.Div,
-}
 
 const checkMark: VirtualDomNode = {
   className: MergeClassNames.mergeClassNames(ClassNames.MenuItemCheckmarkIcon, ClassNames.MaskIconCheck),
@@ -20,6 +11,19 @@ const checkMark: VirtualDomNode = {
 }
 
 export const getMenuItemCheckedDom = (menuItem: VisibleMenuItem): readonly VirtualDomNode[] => {
-  const { label } = menuItem
-  return [checkboxChecked, checkMark, text(label)]
+  const { isFocused, label } = menuItem
+  const baseClassName = MergeClassNames.mergeClassNames(ClassNames.MenuItem, ClassNames.MenuItemCheckMark)
+  const className = isFocused ? mergeClassNames(baseClassName, ClassNames.MenuItemFocused) : baseClassName
+  return [
+    {
+      ariaChecked: true,
+      childCount: 2,
+      className,
+      role: AriaRoles.MenuItemCheckBox,
+      tabIndex: -1,
+      type: VirtualDomElements.Div,
+    },
+    checkMark,
+    text(label),
+  ]
 }
---
+++ packages/title-bar-worker/test/GetMenuItemCheckedDom.test.ts
@@ -0,0 +1,39 @@
+import { expect, test } from '@jest/globals'
+import { AriaRoles, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { VisibleMenuItem } from '../src/parts/VisibleMenuItem/VisibleMenuItem.ts'
+import * as GetMenuItemCheckedDom from '../src/parts/GetMenuItemCheckedDom/GetMenuItemCheckedDom.ts'
+
+const createMenuItem = (isFocused: boolean): VisibleMenuItem => ({
+  flags: 0,
+  isExpanded: false,
+  isFocused,
+  key: 0,
+  label: 'Auto Save',
+  level: 0,
+})
+
+test('getMenuItemCheckedDom - not focused', () => {
+  const result = GetMenuItemCheckedDom.getMenuItemCheckedDom(createMenuItem(false))
+
+  expect(result[0]).toEqual({
+    ariaChecked: true,
+    childCount: 2,
+    className: 'MenuItem MenuItemCheckMark',
+    role: AriaRoles.MenuItemCheckBox,
+    tabIndex: -1,
+    type: VirtualDomElements.Div,
+  })
+})
+
+test('getMenuItemCheckedDom - focused', () => {
+  const result = GetMenuItemCheckedDom.getMenuItemCheckedDom(createMenuItem(true))
+
+  expect(result[0]).toEqual({
+    ariaChecked: true,
+    childCount: 2,
+    className: 'MenuItem MenuItemCheckMark MenuItemFocused',
+    role: AriaRoles.MenuItemCheckBox,
+    tabIndex: -1,
+    type: VirtualDomElements.Div,
+  })
+})
