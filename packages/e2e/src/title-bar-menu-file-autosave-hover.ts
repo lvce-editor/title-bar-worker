5bf9bd8774e2d41abaa2385a48b005f86342cf0d
Le Vivilet <levivilet@pm.me>
2026-07-10 21:52:26 +0000

    fix: enable autosave menu hover

---
+++ packages/e2e/src/title-bar-menu-file-autosave-hover.ts
@@ -0,0 +1,13 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'title-bar-menu-file-autosave-hover'
+
+export const test: Test = async ({ expect, Locator, TitleBarMenuBar }) => {
+  await TitleBarMenuBar.focus()
+  await TitleBarMenuBar.handleKeyArrowDown()
+
+  const autoSaveItem = Locator('#Menu-0 .MenuItem', { hasText: 'Auto Save' })
+  await autoSaveItem.hover()
+
+  await expect(autoSaveItem).toHaveClass('MenuItem MenuItemFocused')
+}
--- packages/title-bar-worker/src/parts/GetMenuItemUncheckedDom/GetMenuItemUncheckedDom.ts
+++ packages/title-bar-worker/src/parts/GetMenuItemUncheckedDom/GetMenuItemUncheckedDom.ts
@@ -1,19 +1,21 @@
-import { VirtualDomElements, AriaRoles } from '@lvce-editor/virtual-dom-worker'
+import { AriaRoles, mergeClassNames, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import type { VisibleMenuItem } from '../VisibleMenuItem/VisibleMenuItem.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
-const checkboxUnchecked: VirtualDomNode = {
-  ariaChecked: false,
-  childCount: 1,
-  className: ClassNames.MenuItem,
-  role: AriaRoles.MenuItemCheckBox,
-  tabIndex: -1,
-  type: VirtualDomElements.Div,
-}
-
 export const getMenuItemUncheckedDom = (menuItem: VisibleMenuItem): readonly VirtualDomNode[] => {
-  const { label } = menuItem
-  return [checkboxUnchecked, text(label)]
+  const { isFocused, label } = menuItem
+  const className = isFocused ? mergeClassNames(ClassNames.MenuItem, ClassNames.MenuItemFocused) : ClassNames.MenuItem
+  return [
+    {
+      ariaChecked: false,
+      childCount: 1,
+      className,
+      role: AriaRoles.MenuItemCheckBox,
+      tabIndex: -1,
+      type: VirtualDomElements.Div,
+    },
+    text(label),
+  ]
 }
