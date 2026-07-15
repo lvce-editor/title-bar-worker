81a9e470d0f73d2805c3840e0f256123951ecb29
Le Vivilet <levivilet@pm.me>
2026-07-15 17:09:26 +0000

    feature: implement go to file menu action

---
+++ packages/e2e/src/title-bar-menu-go-submenu-go-to-file.ts
@@ -0,0 +1,30 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'title-bar-menu-go-submenu-go-to-file'
+
+export const test: Test = async ({ Command, expect, FileSystem, Locator, TitleBarMenuBar, Workspace }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/menu-file.txt`, 'content')
+  await Workspace.setPath(tmpDir)
+
+  await TitleBarMenuBar.focus()
+  await TitleBarMenuBar.handleKeyArrowRight()
+  await TitleBarMenuBar.handleKeyArrowRight()
+  await TitleBarMenuBar.handleKeyArrowRight()
+  await TitleBarMenuBar.handleKeyArrowRight()
+  await TitleBarMenuBar.handleKeyArrowDown()
+
+  const goToFileItem = Locator('.MenuItem', { hasText: 'Go to File...' })
+  await expect(goToFileItem).toBeVisible()
+
+  // act
+  await Command.execute('TitleBar.handleMenuClick', 0, 7)
+
+  // assert
+  const quickPick = Locator('.QuickPick')
+  await expect(quickPick).toBeVisible()
+
+  const quickPickItem = Locator('.QuickPickItemLabel').first()
+  await expect(quickPickItem).toContainText('menu-file.txt')
+}
