5ecca7930df1cd4fdc971ef5eae8b479e8d88607
Le Vivilet <levivilet@pm.me>
2026-07-15 17:56:20 +0000

    fix: isolate go to file e2e test

--- packages/e2e/src/title-bar-menu-go-submenu-go-to-file.ts
+++ packages/e2e/src/title-bar-menu-go-submenu-go-to-file.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'title-bar-menu-go-submenu-go-to-file'
 
-export const test: Test = async ({ Command, expect, FileSystem, Locator, TitleBarMenuBar, Workspace }) => {
+export const test: Test = async ({ Command, expect, FileSystem, Locator, QuickPick, TitleBarMenuBar, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/menu-file.txt`, 'content')
@@ -24,6 +24,7 @@ export const test: Test = async ({ Command, expect, FileSystem, Locator, TitleBa
   // assert
   const quickPick = Locator('.QuickPick')
   await expect(quickPick).toBeVisible()
+  await QuickPick.setValue('menu-file')
 
   const quickPickItem = Locator('.QuickPickItemLabel').first()
   await expect(quickPickItem).toContainText('menu-file.txt')
