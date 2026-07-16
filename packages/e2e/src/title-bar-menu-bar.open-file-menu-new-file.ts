68319ed0af2e604397bc4994cccae4cba54ca025
Le Vivilet <levivilet@pm.me>
2026-07-16 09:27:10 +0000

    feature: test new file menu editor focus

--- packages/e2e/src/title-bar-menu-bar.open-file-menu-new-file.ts
+++ packages/e2e/src/title-bar-menu-bar.open-file-menu-new-file.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'title-bar-menu-bar.open-file-menu-new-file'
 
-export const skip = 1
-
 export const test: Test = async ({ Command, expect, Locator, TitleBarMenuBar }) => {
   // arrange
   const titleBarItemFile = Locator('.TitleBarTopLevelEntry', {
@@ -23,7 +21,10 @@ export const test: Test = async ({ Command, expect, Locator, TitleBarMenuBar }) 
   const menuItemNewFile = Locator('.MenuItem', { hasText: 'New File' })
   await Command.execute('TitleBar.handleMenuClick', 0, 0)
 
-  // assert - verify action was triggered
-  // The new file creation command should have been executed
-  await expect(menuItemNewFile).not.toBeVisible()
+  // assert
+  await expect(menuItemNewFile).toBeHidden()
+  const newFileTab = Locator('.MainTab.MainTabSelected')
+  await expect(newFileTab).toBeVisible()
+  const editorInput = Locator('.EditorInput textarea')
+  await expect(editorInput).toBeFocused()
 }
