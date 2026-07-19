9680c72b5a78f8ee95d379c196a658a424c3dc17
Le Vivilet <levivilet@pm.me>
2026-07-19 17:07:40 +0000

    feature: enable recommended virtual dom lint rules

--- packages/title-bar-worker/src/parts/GetIconVirtualDom/GetIconVirtualDom.ts
+++ packages/title-bar-worker/src/parts/GetIconVirtualDom/GetIconVirtualDom.ts
@@ -1,10 +1,12 @@
 import { VirtualDomElements, AriaRoles } from '@lvce-editor/virtual-dom-worker'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
 export const getIconVirtualDom = (icon: string, type = VirtualDomElements.Div): VirtualDomNode => {
   return {
     childCount: 0,
-    className: `MaskIcon MaskIcon${icon}`,
+    className: MergeClassNames.mergeClassNames(ClassNames.MaskIcon, `MaskIcon${icon}`),
     role: AriaRoles.None,
     type,
   }
