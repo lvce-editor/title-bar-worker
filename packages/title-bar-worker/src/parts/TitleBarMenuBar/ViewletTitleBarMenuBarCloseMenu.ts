fc9e15ac39c338c5ab9a564ed78a6d3b1c8cdf5d
Le Vivilet <levivilet@pm.me>
2026-07-19 21:32:07 +0000

    feature: suppress empty menu close renders

--- packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarCloseMenu.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarCloseMenu.ts
@@ -1,11 +1,14 @@
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 
 export const closeMenu = (state: TitleBarMenuBarState, keepFocus: boolean): TitleBarMenuBarState => {
-  const { focusedIndex } = state
+  const { focusedIndex, isMenuOpen, menus } = state
   // TODO send to renderer process
   // 1. close menu
   // 2. focus top level entry
   const newFocusedIndex = keepFocus ? focusedIndex : -1
+  if (!isMenuOpen && menus.length === 0 && focusedIndex === newFocusedIndex) {
+    return state
+  }
   return {
     ...state,
     focusedIndex: newFocusedIndex,
