84920b664ccc9d44da352a885fe35e7f568c4be5
Le Vivilet <levivilet@pm.me>
2026-08-05 10:43:20 +0000

    fix: detect open text editors for selection menu

---
+++ packages/title-bar-worker/test/HasOpenTextEditor.test.ts
@@ -0,0 +1,61 @@
+import { expect, test } from '@jest/globals'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import { hasOpenTextEditor } from '../src/parts/HasOpenTextEditor/HasOpenTextEditor.ts'
+
+test('hasOpenTextEditor - no open editor', async () => {
+  using mockRpc = RendererWorker.registerMockRpc({
+    'GetActiveEditor.getOpenEditorUris'() {
+      return []
+    },
+  })
+
+  await expect(hasOpenTextEditor()).resolves.toBe(false)
+  expect(mockRpc.invocations).toEqual([['GetActiveEditor.getOpenEditorUris']])
+})
+
+test('hasOpenTextEditor - empty editor URI', async () => {
+  using _mockRpc = RendererWorker.registerMockRpc({
+    'GetActiveEditor.getOpenEditorUris'() {
+      return ['']
+    },
+  })
+
+  await expect(hasOpenTextEditor()).resolves.toBe(false)
+})
+
+test('hasOpenTextEditor - text editor', async () => {
+  using _mockRpc = RendererWorker.registerMockRpc({
+    'GetActiveEditor.getOpenEditorUris'() {
+      return ['memfs:///test/file.txt']
+    },
+  })
+
+  await expect(hasOpenTextEditor()).resolves.toBe(true)
+})
+
+test('hasOpenTextEditor - extension detail', async () => {
+  using _mockRpc = RendererWorker.registerMockRpc({
+    'GetActiveEditor.getOpenEditorUris'() {
+      return ['extension-detail://builtin.cobalt2']
+    },
+  })
+
+  await expect(hasOpenTextEditor()).resolves.toBe(false)
+})
+
+test('hasOpenTextEditor - image editor', async () => {
+  using _mockRpc = RendererWorker.registerMockRpc({
+    'GetActiveEditor.getOpenEditorUris'() {
+      return ['file:///test/image.PNG?version=1']
+    },
+  })
+
+  await expect(hasOpenTextEditor()).resolves.toBe(false)
+})
+
+test('hasOpenTextEditor - unavailable getter', async () => {
+  using mockRpc = RendererWorker.registerMockRpc({})
+
+  await expect(hasOpenTextEditor()).resolves.toBe(false)
+  expect(mockRpc.invocations).toEqual([['GetActiveEditor.getOpenEditorUris']])
+})
