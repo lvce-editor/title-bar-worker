3e061e66783b7e0a6dea9975c6cc04dc74291f94
Le Vivilet <levivilet@pm.me>
2026-08-05 07:49:16 -0700

    fix: preserve workspace title after open recent (#653)

--- packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarSelectIndexNone.ts
+++ packages/title-bar-worker/src/parts/TitleBarMenuBar/ViewletTitleBarMenuBarSelectIndexNone.ts
@@ -1,11 +1,14 @@
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 import type { TitleBarMenuBarState } from '../TitleBarMenuBarState/TitleBarMenuBarState.ts'
 import * as ExecuteMenuItemcommand from '../ExecuteMenuItemCommand/ExecuteMenuItemCommand.ts'
+import * as TitleBarMenuBarStates from '../TitleBarMenuBarStates/TitleBarMenuBarStates.ts'
 
 export const selectIndexNone = async (state: TitleBarMenuBarState, item: MenuEntry): Promise<TitleBarMenuBarState> => {
+  const { uid } = state
   await ExecuteMenuItemcommand.executeMenuItemCommand(item)
+  const latestState = TitleBarMenuBarStates.get(uid)?.newState ?? state
   return {
-    ...state,
+    ...latestState,
     focusedIndex: -1,
     isMenuOpen: false,
     menus: [],
