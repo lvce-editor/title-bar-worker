d338a13ea1c1f10254b01f7013c444fce0689d79
Le Vivilet <levivilet@pm.me>
2026-08-24 20:24:06 +0000

    test: focus title bar highlight regression

--- packages/e2e/src/title-bar-menu-clear-highlight-on-editor-click.ts
+++ packages/e2e/src/title-bar-menu-clear-highlight-on-editor-click.ts
@@ -2,20 +2,19 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'title-bar-menu-clear-highlight-on-editor-click'
 
-export const test: Test = async ({ expect, FileSystem, KeyBoard, Locator, Main }) => {
+export const test: Test = async ({ expect, FileSystem, Locator, Main, TitleBarMenuBar }) => {
   const tmpDir = await FileSystem.getTmpDir()
   const testFile = `${tmpDir}/clear-title-bar-highlight.txt`
   await FileSystem.writeFile(testFile, 'hello')
   await Main.openUri(testFile)
 
   const titleBarItemFile = Locator('.TitleBarTopLevelEntry', { hasText: 'File' })
-  // eslint-disable-next-line e2e/no-direct-click
-  await titleBarItemFile.click()
+  await TitleBarMenuBar.toggleIndex(0)
 
   const menu = Locator('#Menu-0')
   await expect(menu).toBeVisible()
 
-  await KeyBoard.press('Escape')
+  await TitleBarMenuBar.handleKeyEscape()
 
   await expect(menu).toBeHidden()
   await expect(titleBarItemFile).toHaveAttribute('id', 'TitleBarEntryActive')
