5c93858c6c466298df6a11c7be9610833bcf288e
Le Vivilet <levivilet@pm.me>
2026-09-04 09:18:08 +0000

    fix: open command palette from view menu (#675)

--- packages/e2e/src/title-bar-menu-view-command-palette.ts
+++ packages/e2e/src/title-bar-menu-view-command-palette.ts
@@ -1,4 +1,12 @@
-import { createMenuItemTest } from './shared/menuItem.ts'
+import type { Test } from '@lvce-editor/test-with-playwright'
+import { selectViewMenuItem } from './shared/viewMenu.ts'
 
 export const name = 'title-bar-menu-view-command-palette'
-export const test = createMenuItemTest(3, 0, 'Command Palette')
+
+export const test: Test = async (api) => {
+  await selectViewMenuItem(api, 0, 'Command Palette')
+
+  await api.expect(api.Locator('.QuickPick')).toBeVisible()
+  await api.expect(api.Locator('[name="QuickPickInput"]')).toHaveValue('>')
+  await api.Command.execute('QuickPick.close')
+}
